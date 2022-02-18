500ae5d21861a2fe22344e25ad1167417991f439
Andy Langridge <andy.langridge@kinandcarta.com>
2022-02-18 11:06:38 +0000

    Add additional provider configuration for tooling subscription

--- app/stacks/appeals-service/data.tf
+++ app/stacks/appeals-service/data.tf
@@ -1,4 +1,6 @@
 data "azurerm_container_registry" "acr" {
   name                = var.container_registry_name
   resource_group_name = var.container_registry_rg
+
+  provider = azurerm.tooling
 }
--- config/provider.tf
+++ config/provider.tf
@@ -1,9 +1,3 @@
-# variable "subscription_id" {
-#   description = "The subscription ID of the subscription terraform is deploying to"
-#   type        = string
-# }
-
-
 terraform {
   required_providers {
     azurerm = {
@@ -15,5 +9,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  #  subscription_id = var.subscription_id
 }
