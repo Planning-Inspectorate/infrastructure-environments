09443c6668bed7bc68e853e54b6f3feafa49a5fc
Andy Langridge <andy.langridge@kinandcarta.com>
2022-03-08 15:23:17 +0000

    Formatting

--- app/stacks/common/data.tf
+++ app/stacks/common/data.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_key_vault_secret" "applications_service_vpn_gateway_shared_key" {
-  name = azurerm_key_vault_secret.applications_service_vpn_gateway_shared_key.name
+  name         = azurerm_key_vault_secret.applications_service_vpn_gateway_shared_key.name
   key_vault_id = azurerm_key_vault.environment_key_vault.id
 }
 
