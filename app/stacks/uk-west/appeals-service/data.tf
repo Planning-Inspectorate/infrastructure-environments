ce7b1607de0cd9d2ff0ddca1958991636123a1a0
Andy Langridge <andy.langridge@kinandcarta.com>
2022-03-18 21:05:55 +0000

    Add missing providers

--- app/stacks/uk-west/appeals-service/data.tf
+++ app/stacks/uk-west/appeals-service/data.tf
@@ -7,8 +7,12 @@ data "azurerm_container_registry" "acr" {
 
 data "azurerm_private_dns_zone" "app_service" {
   name = "privatelink.azurewebsites.net"
+
+  provider = azurerm.tooling
 }
 
 data "azurerm_private_dns_zone" "cosmosdb" {
   name = "privatelink.mongo.cosmos.azure.com"
+
+  provider = azurerm.tooling
 }
