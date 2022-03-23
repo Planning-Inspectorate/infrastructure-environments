c230b4c92de0ea881084abf9aa54635b5f92132c
Andy Langridge <andy.langridge@kinandcarta.com>
2022-03-23 11:49:07 +0000

    Put WAF in detection mode

--- app/stacks/global/front-door/waf-policy.tf
+++ app/stacks/global/front-door/waf-policy.tf
@@ -2,7 +2,7 @@ resource "azurerm_frontdoor_firewall_policy" "default" {
   name                = replace("pinswaf${local.service_name}${local.resource_suffix}", "-", "")
   resource_group_name = azurerm_resource_group.frontdoor.name
   enabled             = true
-  mode                = "Prevention"
+  mode                = "Detection"
 
   managed_rule {
     type    = "DefaultRuleSet"
