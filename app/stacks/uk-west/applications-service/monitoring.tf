c86826c3a88e83dda35e5f12f77f4e8a0fde34af
Lester March <lestermarch@outlook.com>
2022-03-28 18:12:46 +0100

    Dynamise metric alert

--- app/stacks/uk-west/applications-service/monitoring.tf
+++ app/stacks/uk-west/applications-service/monitoring.tf
@@ -1,7 +1,9 @@
 resource "azurerm_monitor_metric_alert" "http_5xx" {
+  for_each = toset(module.app_services.app_service_ids)
+
   name                = "HttpResponse5xx"
   resource_group_name = azurerm_resource_group.applications_service_stack.name
-  scopes              = module.app_services.app_service_ids
+  scopes              = [each.key]
   description         = "Sends an alert when app services return excess 5xx respones"
   window_size         = "PT5M"
   frequency           = "PT1M"
