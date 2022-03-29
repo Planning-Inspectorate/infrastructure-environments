c0ef7c6a4e1d0ca009ed68cf3146e17d18074334
Lester March <lestermarch@outlook.com>
2022-03-29 10:21:39 +0100

    Additional monitor and activity alerts

--- app/stacks/uk-west/applications-service/monitoring.tf
+++ app/stacks/uk-west/applications-service/monitoring.tf
@@ -1,27 +1,52 @@
-# resource "azurerm_monitor_metric_alert" "http_5xx" {
-#   for_each = toset(module.app_services.app_service_ids)
+resource "azurerm_monitor_metric_alert" "app_service_http_4xx" {
+  for_each = toset(module.app_services.app_service_ids)
 
-#   name                = "Http 5xx - ${reverse(split("/", each.key))[0]}"
-#   resource_group_name = azurerm_resource_group.applications_service_stack.name
-#   scopes              = [each.key]
-#   description         = "Sends an alert when the App Service returns excess 5xx respones"
-#   window_size         = "PT5M"
-#   frequency           = "PT1M"
-#   severity            = 4
-#   tags                = var.common_tags
+  name                = "Http 4xx - ${reverse(split("/", each.key))[0]}"
+  resource_group_name = azurerm_resource_group.applications_service_stack.name
+  scopes              = [each.key]
+  description         = "Sends an alert when the App Service returns excess 4xx respones"
+  window_size         = "PT5M"
+  frequency           = "PT1M"
+  severity            = 4
+  tags                = var.common_tags
 
-#   criteria {
-#     metric_namespace = "Microsoft.Web/sites"
-#     metric_name      = "Http5xx"
-#     aggregation      = "Count"
-#     operator         = "GreaterThan"
-#     threshold        = 10
-#   }
+  criteria {
+    metric_namespace = "Microsoft.Web/sites"
+    metric_name      = "Http4xx"
+    aggregation      = "Average"
+    operator         = "GreaterThanOrEqual"
+    threshold        = 1
+  }
 
-#   action {
-#     action_group_id = var.action_group_low_id
-#   }
-# }
+  action {
+    action_group_id = var.action_group_low_id
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "app_service_http_5xx" {
+  for_each = toset(module.app_services.app_service_ids)
+
+  name                = "Http 5xx - ${reverse(split("/", each.key))[0]}"
+  resource_group_name = azurerm_resource_group.applications_service_stack.name
+  scopes              = [each.key]
+  description         = "Sends an alert when the App Service returns excess 5xx respones"
+  window_size         = "PT5M"
+  frequency           = "PT1M"
+  severity            = 4
+  tags                = var.common_tags
+
+  criteria {
+    metric_namespace = "Microsoft.Web/sites"
+    metric_name      = "Http5xx"
+    aggregation      = "Average"
+    operator         = "GreaterThanOrEqual"
+    threshold        = 1
+  }
+
+  action {
+    action_group_id = var.action_group_low_id
+  }
+}
 
 resource "azurerm_monitor_metric_alert" "app_service_response_time" {
   for_each = toset(module.app_services.app_service_ids)
@@ -61,6 +86,26 @@ resource "azurerm_monitor_activity_log_alert" "app_service_stop" {
     resource_id    = each.key
     category       = "Administrative"
     operation_name = "Microsoft.Web/sites/stop/Action"
+  }
+
+  action {
+    action_group_id = var.action_group_low_id
+  }
+}
+
+resource "azurerm_monitor_activity_log_alert" "app_service_delete" {
+  for_each = toset(module.app_services.app_service_ids)
+
+  name                = "App Service Deleted - ${reverse(split("/", each.key))[0]}"
+  resource_group_name = azurerm_resource_group.applications_service_stack.name
+  scopes              = [each.key]
+  description         = "Sends an alert when the App Service is deleted"
+  tags                = var.common_tags
+
+  criteria {
+    resource_id    = each.key
+    category       = "Administrative"
+    operation_name = "Microsoft.Web/sites/Delete"
   }
 
   action {
