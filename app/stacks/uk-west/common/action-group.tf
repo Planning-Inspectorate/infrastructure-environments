8a0a236aedd8deafa6dee2091fc3dca2b861ed98
Lester March <lestermarch@outlook.com>
2022-04-01 17:28:22 +0100

    Update low severity action group recipients

--- app/stacks/uk-west/common/action-group.tf
+++ app/stacks/uk-west/common/action-group.tf
@@ -1,12 +1,36 @@
 resource "azurerm_monitor_action_group" "low" {
   name                = "pins-ag-low-${local.service_name}-${local.resource_suffix}"
   resource_group_name = azurerm_resource_group.common_infrastructure.name
-  short_name          = "P4-${var.environment}"
+  short_name          = "pins-${var.environment}-low"
   tags                = local.tags
 
   email_receiver {
-    name                    = "P4"
+    name                    = "Send to Lester March"
     email_address           = "lester.march@kinandcarta.com"
+    use_common_alert_schema = true
+  }
+
+  email_receiver {
+    name                    = "Send to Chris Cundill"
+    email_address           = "chris.cundill@planninginspectorate.gov.uk"
+    use_common_alert_schema = true
+  }
+
+  email_receiver {
+    name                    = "Send to Elvin Ali"
+    email_address           = "elvin.ali@planninginspectorate.gov.uk"
+    use_common_alert_schema = true
+  }
+
+  email_receiver {
+    name                    = "Send to Pankaj Khare"
+    email_address           = "pankaj.khare@planninginspectorate.gov.uk"
+    use_common_alert_schema = true
+  }
+
+  email_receiver {
+    name                    = "Send to Harish Kancharla"
+    email_address           = "harish.kancharla@planninginspectorate.gov.uk"
     use_common_alert_schema = true
   }
 }
