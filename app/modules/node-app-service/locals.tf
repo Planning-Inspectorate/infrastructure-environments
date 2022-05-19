49d139666d3e29faae777e1dd82aeeb848cbde22
Andy Langridge <andy.langridg3@gmail.com>
2022-05-19 10:15:09 +0100

    Fix Container Registry web hook scope  (#88)
    * Fix web hook scope
    
    * Enable CI for web apps
    
    Co-authored-by: Andy Langridge <andy.langridge@kinandcarta.com>

--- app/modules/node-app-service/locals.tf
+++ app/modules/node-app-service/locals.tf
@@ -3,6 +3,7 @@ locals {
     APPINSIGHTS_INSTRUMENTATIONKEY             = var.app_insights_instrumentation_key
     APPLICATIONINSIGHTS_CONNECTION_STRING      = var.app_insights_connection_string
     ApplicationInsightsAgent_EXTENSION_VERSION = "~3"
+    DOCKER_ENABLE_CI                           = true
     DOCKER_REGISTRY_SERVER_PASSWORD            = data.azurerm_container_registry.acr.admin_password
     DOCKER_REGISTRY_SERVER_URL                 = data.azurerm_container_registry.acr.login_server
     DOCKER_REGISTRY_SERVER_USERNAME            = data.azurerm_container_registry.acr.admin_username
