f7c0b8314a6ae19eb44780698b2726ede6f87dbf
Andy Langridge <andy.langridge@kinandcarta.com>
2022-05-26 11:00:39 +0100

    Mark registry password as sensitive

--- app/modules/node-app-service/locals.tf
+++ app/modules/node-app-service/locals.tf
@@ -4,7 +4,7 @@ locals {
     APPLICATIONINSIGHTS_CONNECTION_STRING      = var.app_insights_connection_string
     ApplicationInsightsAgent_EXTENSION_VERSION = "~3"
     DOCKER_ENABLE_CI                           = true
-    DOCKER_REGISTRY_SERVER_PASSWORD            = data.azurerm_container_registry.acr.admin_password
+    DOCKER_REGISTRY_SERVER_PASSWORD            = sensitive(data.azurerm_container_registry.acr.admin_password)
     DOCKER_REGISTRY_SERVER_URL                 = data.azurerm_container_registry.acr.login_server
     DOCKER_REGISTRY_SERVER_USERNAME            = data.azurerm_container_registry.acr.admin_username
     XDT_MicrosoftApplicationInsights_Mode      = "default"
