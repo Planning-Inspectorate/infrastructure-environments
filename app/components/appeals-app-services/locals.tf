fbe4f6bc9d3f96ba67e6c441b1a8e1b63dbfba42
Lester March <lestermarch@outlook.com>
2022-06-06 16:17:16 +0100

    Use service bus auth rule primary key output

--- app/components/appeals-app-services/locals.tf
+++ app/components/appeals-app-services/locals.tf
@@ -64,7 +64,7 @@ locals {
         FEATURE_FLAG_NEW_APPEAL_JOURNEY                                       = true
         HORIZON_HAS_PUBLISHER_ATTEMPT_RECONNECTION                            = true
         HORIZON_HAS_PUBLISHER_HOST                                            = "${azurerm_servicebus_namespace.horizon.name}.servicebus.windows.net"
-        HORIZON_HAS_PUBLISHER_PASSWORD                                        = split("SharedAccessKey=", azurerm_servicebus_namespace_authorization_rule.horizon_function_apps.primary_connection_string)[1]
+        HORIZON_HAS_PUBLISHER_PASSWORD                                        = azurerm_servicebus_namespace_authorization_rule.horizon_function_apps.primary_key
         HORIZON_HAS_PUBLISHER_PORT                                            = "5671"
         HORIZON_HAS_PUBLISHER_QUEUE                                           = azurerm_servicebus_queue.horizon_householder_appeal_publish.name
         HORIZON_HAS_PUBLISHER_RECONNECT_LIMIT                                 = "5"
