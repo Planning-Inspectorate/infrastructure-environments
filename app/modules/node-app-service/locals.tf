af6ad13e5d20e4df857b8d54b67fbaad09e688a3
Andy Langridge <andy.langridge@kinandcarta.com>
2022-07-05 11:27:08 +0100

    Remove staging slot settings and use values from production slot

--- app/modules/node-app-service/locals.tf
+++ app/modules/node-app-service/locals.tf
@@ -8,16 +8,4 @@ locals {
       DOCKER_REGISTRY_SERVER_USERNAME = data.azurerm_container_registry.acr.admin_username
     }
   )
-
-  # Since some App Settings will be URLs to other Azure App Services, this local variable parses each one and does the following:
-  # 1. Check if the setting contains an App Service URL. We do this by looking for the '<resource_suffix.azurewebsites.net' string
-  # 2. Remove '.azurewebsites.net' string from the end of the URL
-  # 3. Truncate the string to 48 characters (this is the number of characters including https:// that Azure use before adding the staging slot in the URL)
-  # 4. If the last character after truncation is a '-' remove this too
-  # 5. Add "-staging.azurewebsites.net" back at the end of the string to complete the URL
-  # 6. Add a '/' character back at the end if the original App Setting included this
-  # The final result will be a URL of a staging slot
-  staging_slot_app_settings = {
-    for k, v in local.app_settings : k => length(regexall("${var.resource_suffix}\\.azurewebsites\\.net\\/?$", v)) > 0 ? length(regexall("\\/$", v)) > 0 ? "${trimsuffix(substr(replace(v, ".azurewebsites.net/", ""), 0, 48), "-")}-staging.azurewebsites.net/" : "${trimsuffix(substr(replace(v, ".azurewebsites.net", ""), 0, 48), "-")}-staging.azurewebsites.net" : v
-  }
 }
