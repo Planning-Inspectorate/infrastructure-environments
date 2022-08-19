0a12717486988b01fa845bb7737d7b4261242d55
Mayya Bondarevskaya <89029990+mayya-bond-kc@users.noreply.github.com>
2022-08-19 14:21:21 +0100

    feat(back-office): adds blob store and service bus (#129)
    * feat(back-office): adds blob store and service bus
    
    * chore(back-office): format terraform file
    
    * feat(back-office): moves service bus config to stacks
    
    * feat(back-office): adds second service bus for disaster recovery

---
+++ app/stacks/uk-west/back-office/service-bus.tf
@@ -0,0 +1,15 @@
+resource "azurerm_servicebus_namespace" "back_office" {
+  name                = "pins-sb-${local.service_name}-${local.resource_suffix}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.back_office_stack.name
+  sku                 = "Standard"
+
+  tags = local.tags
+}
+
+resource "azurerm_servicebus_queue" "back_office_publish" {
+  name         = "back-office-publish"
+  namespace_id = azurerm_servicebus_namespace.back_office.id
+
+  enable_partitioning = true
+}
