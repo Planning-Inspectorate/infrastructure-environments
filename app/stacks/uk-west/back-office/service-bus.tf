2e7a15481893997b644d51a33fe89f4e271ad660
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2022-08-22 16:22:59 +0100

    feat(back-office): refactors location to use azurerm_resource_group.back_office_stack.location

--- app/stacks/uk-west/back-office/service-bus.tf
+++ app/stacks/uk-west/back-office/service-bus.tf
@@ -1,6 +1,6 @@
 resource "azurerm_servicebus_namespace" "back_office" {
   name                = "pins-sb-${local.service_name}-${local.resource_suffix}"
-  location            = var.location
+  location            = azurerm_resource_group.back_office_stack.location
   resource_group_name = azurerm_resource_group.back_office_stack.name
   sku                 = "Standard"
 
