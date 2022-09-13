029e474485eb364791f6c5c5212028b01e3a1b0f
Lester March <lestermarch@outlook.com>
2022-09-13 15:39:24 +0100

    fix(back-office-api): set key vault access to true

--- app/components/back-office-app-services/locals.tf
+++ app/components/back-office-app-services/locals.tf
@@ -36,7 +36,7 @@ locals {
       image_name                      = "back-office/back-office-api"
       inbound_vnet_connectivity       = var.private_endpoint_enabled
       integration_subnet_id           = var.integration_subnet_id
-      key_vault_access                = false
+      key_vault_access                = true
       outbound_vnet_connectivity      = true
 
       app_settings = {
