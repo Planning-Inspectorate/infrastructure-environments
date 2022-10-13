e2ced5c009a960ffad89e4a258b487ff9643d75e
Lester March <lestermarch@outlook.com>
2022-10-13 09:12:47 +0100

    feat(appeals): increase sku for app config failover instance

--- app/stacks/uk-south/appeals-service/app-service-config.tf
+++ app/stacks/uk-south/appeals-service/app-service-config.tf
@@ -4,4 +4,7 @@ resource "azurerm_app_configuration" "appeals_service" {
   name                = "pins-asc-${local.service_name}-${local.resource_suffix}"
   resource_group_name = azurerm_resource_group.appeals_service_stack.name
   location            = module.azure_region_uks.location
+  sku                 = "standard"
+
+  tags = local.tags
 }
