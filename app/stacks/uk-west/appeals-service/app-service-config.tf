de260112bc9ffa280a6186abd814e38fc27c5cb1
Lester March <lestermarch@outlook.com>
2022-10-13 09:06:57 +0100

    fix(appeals): change app configuration name

--- app/stacks/uk-west/appeals-service/app-service-config.tf
+++ app/stacks/uk-west/appeals-service/app-service-config.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_configuration" "appeals_service" {
-  name                = "pins-asc-${local.service_name}-${local.resource_suffix}"
+  name                = "pins-asc-appeals-${local.resource_suffix}"
   resource_group_name = azurerm_resource_group.appeals_service_stack.name
   location            = module.azure_region_primary.location
   sku                 = "standard"
