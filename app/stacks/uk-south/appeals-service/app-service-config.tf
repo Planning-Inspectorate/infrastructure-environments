297d00de4fb67bd97c5cfaa1a08bfb9998e55a23
Lester March <lestermarch@outlook.com>
2022-10-13 09:16:13 +0100

    fix(appeals): use correct name for app config failover instance

--- app/stacks/uk-south/appeals-service/app-service-config.tf
+++ app/stacks/uk-south/appeals-service/app-service-config.tf
@@ -1,7 +1,7 @@
 resource "azurerm_app_configuration" "appeals_service" {
   count = var.is_dr_deployment ? 1 : 0
 
-  name                = "pins-asc-${local.service_name}-${local.resource_suffix}"
+  name                = "pins-asc-appeals-${local.resource_suffix}"
   resource_group_name = azurerm_resource_group.appeals_service_stack.name
   location            = module.azure_region_uks.location
   sku                 = "standard"
