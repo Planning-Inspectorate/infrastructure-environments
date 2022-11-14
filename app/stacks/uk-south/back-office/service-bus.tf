ed1ad31b7e5dc022b73c1a48ab7740155ab48b01
Lester March <lestermarch@outlook.com>
2022-11-14 11:37:59 +0000

    feat(back-office): add failover condition for secondary service bus

--- app/stacks/uk-south/back-office/service-bus.tf
+++ app/stacks/uk-south/back-office/service-bus.tf
@@ -1,4 +1,6 @@
 resource "azurerm_servicebus_namespace" "back_office" {
+  count = var.service_bus_failover_enabled || var.is_dr_deployment ? 1 : 0
+
   name                = "pins-sb-${local.service_name}-${local.resource_suffix}"
   location            = azurerm_resource_group.back_office_stack.location
   resource_group_name = azurerm_resource_group.back_office_stack.name
@@ -13,5 +15,5 @@ resource "azurerm_servicebus_namespace_disaster_recovery_config" "back_office" {
 
   name                 = "pins-sb-dr-${local.service_name}-${local.resource_suffix}"
   primary_namespace_id = var.primary_service_bus_namespace_id
-  partner_namespace_id = azurerm_servicebus_namespace.back_office.id
+  partner_namespace_id = azurerm_servicebus_namespace.back_office[0].id
 }
