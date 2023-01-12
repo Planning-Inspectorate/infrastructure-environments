312ea2721917bb16a22adc6470a52e5f6f4904f6
Christopher Daly <christopher.daly@kinandcarta.com>
2023-01-12 10:19:05 +0000

    BOAS-687 Add sleep for debugging

--- app/components/back-office-app-services/clamav-container/container.tf
+++ app/components/back-office-app-services/clamav-container/container.tf
@@ -42,7 +42,7 @@ resource "azurerm_container_group" "back_office_containers" {
     cpu    = "0.5"
     memory = "1.5"
 
-    commands = ["/bin/sh", "-c", "az login --identity; az network private-dns record-set a update --resource-group ${var.resource_group_name} --zone-name ${local.domain_name} --name ${local.dns_record_name} --set \"aRecords[0].ipv4Address=$(hostname -i)\""]
+    commands = ["/bin/sh", "-c", "az login --identity; az network private-dns record-set a update --resource-group ${var.resource_group_name} --zone-name ${local.domain_name} --name ${local.dns_record_name} --set \"aRecords[0].ipv4Address=$(hostname -i)\"; sleep 100000"]
   }
 
   exposed_port {
