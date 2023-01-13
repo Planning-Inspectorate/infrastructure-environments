65868fb7605b9272bba2a2c6015fe9a7126e77f8
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-01-13 16:00:16 +0000

    fix: obtains module.app_service

--- app/components/back-office-app-services/document-check-function/variables.tf
+++ app/components/back-office-app-services/document-check-function/variables.tf
@@ -9,10 +9,10 @@ variable "back_office_document_storage_connection_string" {
   sensitive   = true
 }
 
-variable "back_office_api_host" {
-  description = "Back Office Api Host"
-  type        = string
-}
+# variable "back_office_api_host" {
+#   description = "Back Office Api Host"
+#   type        = string
+# }
 
 variable "location" {
   description = "The location the App Services are deployed to in slug format e.g. 'uk-south'"
