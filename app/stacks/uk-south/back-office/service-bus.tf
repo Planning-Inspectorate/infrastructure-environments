0cd3fd208f50d1e2e00ccb7f1674e180a1851182
Christopher Daly <96062341+chrisdalykinandcarta@users.noreply.github.com>
2023-01-30 14:04:26 +0000

    BOAS-720 Implement private endpoints for SB (#178)

--- app/stacks/uk-south/back-office/service-bus.tf
+++ app/stacks/uk-south/back-office/service-bus.tf
@@ -9,11 +9,52 @@ resource "azurerm_servicebus_namespace" "back_office" {
 
   tags = local.tags
 }
-
 resource "azurerm_servicebus_namespace_disaster_recovery_config" "back_office" {
   count = var.service_bus_failover_enabled ? 1 : 0
 
   name                 = "pins-sb-dr-${local.service_name}-${local.resource_suffix}"
   primary_namespace_id = var.primary_service_bus_namespace_id
   partner_namespace_id = azurerm_servicebus_namespace.back_office[0].id
+}
+
+# https://learn.microsoft.com/en-us/azure/service-bus-messaging/service-bus-geo-dr#recommended-configuration
+# Might be worth looking into this, for now let's just create a private link to the secondary NS
+# Create a private DNS zone for the SB private endpoint
+resource "azurerm_private_dns_zone" "back_office" {
+  count = var.service_bus_failover_enabled ? 1 : 0
+
+  name                = "privatelink.servicebus.windows.net"
+  resource_group_name = azurerm_resource_group.back_office_stack.name
+}
+
+# Create a network ruleset to disable public access
+resource "azurerm_servicebus_namespace_network_rule_set" "back_office" {
+  count = var.service_bus_failover_enabled ? 0 : 1
+
+  namespace_id                  = azurerm_servicebus_namespace.back_office[0].id
+  public_network_access_enabled = false
+}
+
+# Create a private endpoint for the namespace
+resource "azurerm_private_endpoint" "back_office" {
+  count = var.service_bus_failover_enabled ? 1 : 0
+
+  name                = "pins-pe-${local.service_name}-sb-${local.resource_suffix}"
+  location            = azurerm_resource_group.back_office_stack.location
+  resource_group_name = azurerm_resource_group.back_office_stack.name
+  subnet_id           = azurerm_subnet.back_office_ingress.id
+
+  private_dns_zone_group {
+    name                 = "pins-pdns-${local.service_name}-sb-${local.resource_suffix}"
+    private_dns_zone_ids = [azurerm_private_dns_zone.back_office[0].id]
+  }
+
+  private_service_connection {
+    name                           = "pins-psc-${local.service_name}-sb-${local.resource_suffix}"
+    private_connection_resource_id = azurerm_servicebus_namespace.back_office[0].id
+    is_manual_connection           = false
+    subresource_names              = ["namespace"]
+  }
+
+  tags = local.tags
 }
--- app/stacks/uk-west/back-office/service-bus.tf
+++ app/stacks/uk-west/back-office/service-bus.tf
@@ -8,25 +8,44 @@ resource "azurerm_servicebus_namespace" "back_office" {
   tags = local.tags
 }
 
-# case-started
-resource "azurerm_servicebus_topic" "case_started" {
-  name         = "case-started"
-  namespace_id = azurerm_servicebus_namespace.back_office.id
+# Create a private DNS zone for the SB private endpoint
+resource "azurerm_private_dns_zone" "back_office" {
+  count = var.service_bus_failover_enabled ? 1 : 0
+
+  name                = "privatelink.servicebus.windows.net"
+  resource_group_name = azurerm_resource_group.back_office_stack.name
 }
 
-resource "azurerm_servicebus_subscription" "back_office_api" {
-  name               = "back-office-api-subscriber"
-  topic_id           = azurerm_servicebus_topic.case_started.id
-  max_delivery_count = 1
+# Create a network ruleset to disable public access
+resource "azurerm_servicebus_namespace_network_rule_set" "back_office" {
+  count = var.service_bus_failover_enabled ? 0 : 1
+
+  namespace_id                  = azurerm_servicebus_namespace.back_office.id
+  public_network_access_enabled = false
 }
 
-resource "azurerm_servicebus_namespace_authorization_rule" "back_office_apps" {
-  name         = "back-office-apps"
-  namespace_id = azurerm_servicebus_namespace.back_office.id
+# Create a private endpoint for the namespace
+resource "azurerm_private_endpoint" "back_office" {
+  count = var.service_bus_failover_enabled ? 1 : 0
 
-  listen = true
-  send   = true
-  manage = false
+  name                = "pins-pe-${local.service_name}-sb-${local.resource_suffix}"
+  location            = azurerm_resource_group.back_office_stack.location
+  resource_group_name = azurerm_resource_group.back_office_stack.name
+  subnet_id           = azurerm_subnet.back_office_ingress.id
+
+  private_dns_zone_group {
+    name                 = "pins-pdns-${local.service_name}-sb-${local.resource_suffix}"
+    private_dns_zone_ids = [azurerm_private_dns_zone.back_office[0].id]
+  }
+
+  private_service_connection {
+    name                           = "pins-psc-${local.service_name}-sb-${local.resource_suffix}"
+    private_connection_resource_id = azurerm_servicebus_namespace.back_office.id
+    is_manual_connection           = false
+    subresource_names              = ["namespace"]
+  }
+
+  tags = local.tags
 }
 
 # NSIP project
