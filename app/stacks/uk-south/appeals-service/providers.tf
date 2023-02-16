2fe62b3fa1f6a780386fb1f01854e24f1c3336c2
adelany <alex.delany@optima-cc.com>
2023-02-16 12:25:25 +0000

    Updated keyvault secret with expire date

--- app/stacks/uk-south/appeals-service/providers.tf
+++ app/stacks/uk-south/appeals-service/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.6.0"
     }
+    time = {
+      source  = "hashicorp/time"
+      version = "~>0.9"
+    }
   }
 
   required_version = "1.1.6"
