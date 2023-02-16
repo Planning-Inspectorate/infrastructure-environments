1eae636021bc06d252b49203e656ee10756ec7dd
adelany <alex.delany@optima-cc.com>
2023-02-16 16:42:43 +0000

    Remove secret.tf from appeals stop blank secrets

--- app/stacks/uk-west/appeals-service/secrets.tf
+++ app/stacks/uk-west/appeals-service/secrets.tf
@@ -1,18 +1,18 @@
-resource "azurerm_key_vault_secret" "app_secret" {
-  #checkov:skip=CKV_AZURE_41: TODO: Secret rotation
-  #checkov:skip=CKV_AZURE_114: No need to set content type via Terraform, as secrets to be updated in Portal
-  for_each = toset(module.app_services.secret_names)
+# resource "azurerm_key_vault_secret" "app_secret" {
+#   #checkov:skip=CKV_AZURE_41: TODO: Secret rotation
+#   #checkov:skip=CKV_AZURE_114: No need to set content type via Terraform, as secrets to be updated in Portal
+#   for_each = toset(module.app_services.secret_names)
 
-  key_vault_id = var.key_vault_id
-  name         = each.value
-  value        = "<enter_value>"
+#   key_vault_id = var.key_vault_id
+#   name         = each.value
+#   value        = "<enter_value>"
 
-  tags = local.tags
+#   tags = local.tags
 
-  lifecycle {
-    ignore_changes = [
-      value,
-      version
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       value,
+#       version
+#     ]
+#   }
+# }
