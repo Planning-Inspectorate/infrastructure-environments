1ec4414a1ba6845e68c987ad886c34f428956ba8
adelany <alex.delany@optima-cc.com>
2023-02-16 12:25:25 +0000

    Updated keyvault secret with expire date

--- app/stacks/uk-south/appeals-service/app-service-config.tf
+++ app/stacks/uk-south/appeals-service/app-service-config.tf
@@ -32,8 +32,14 @@ resource "azurerm_private_endpoint" "appeals_app_config" {
   tags = local.tags
 }
 
+resource "time_offset" "secret_expire_date" {
+  offset_years = 1
+}
+
 resource "azurerm_key_vault_secret" "appeals_app_config_endpoint_kv_secret" {
-  name         = "appeals-app-config-endpoint"
-  value        = azurerm_app_configuration.appeals_service.primary_write_key[0].connection_string
-  key_vault_id = var.key_vault_id
+  name            = "appeals-app-config-endpoint"
+  value           = azurerm_app_configuration.appeals_service.primary_write_key[0].connection_string
+  key_vault_id    = var.key_vault_id
+  content_type    = "text/plain"
+  expiration_date = time_offset.secret_expire_date.rfc3339
 }
