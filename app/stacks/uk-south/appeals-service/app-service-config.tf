b3dd3f1dfb6577880a8616995871e34d812af95a
adelany <alex.delany@optima-cc.com>
2023-02-16 13:13:23 +0000

    Add count to uk south appeals keyvault secret

--- app/stacks/uk-south/appeals-service/app-service-config.tf
+++ app/stacks/uk-south/appeals-service/app-service-config.tf
@@ -37,6 +37,7 @@ resource "time_offset" "secret_expire_date" {
 }
 
 resource "azurerm_key_vault_secret" "appeals_app_config_endpoint_kv_secret" {
+  count           = var.is_dr_deployment ? 1 : 0
   name            = "appeals-app-config-endpoint"
   value           = azurerm_app_configuration.appeals_service[0].primary_write_key[0].connection_string
   key_vault_id    = var.key_vault_id
