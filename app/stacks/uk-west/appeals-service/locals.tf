dc6e02a7ca987d3a756e2a1e714522ec9f17ba33
adelany <alex.delany@optima-cc.com>
2023-02-28 10:18:47 +0000

    Fixed local.tf syntax for appeals

--- app/stacks/uk-west/appeals-service/locals.tf
+++ app/stacks/uk-west/appeals-service/locals.tf
@@ -2,6 +2,15 @@ locals {
   service_name    = "appeals-service"
   resource_suffix = "${var.environment}-${module.azure_region_primary.location_short}-${var.instance}"
 
+  secret_names_exclude = [
+    "appeals-app-config-connection-string"
+  ]
+  secret_names = [
+    for secret in module.app_services.secrets :
+    secret.name
+    if !contains(local.secret_names_exclude, secret.name)
+  ]
+
   tags = merge(
     var.common_tags,
     {
@@ -9,15 +18,4 @@ locals {
       ServiceName = local.service_name
     }
   )
-}
-
-locals { 
-  secret_names_exclude = [
-    "appeals-app-config-connection-string"
-  ]
-  secret_names = [
-    for secret in module.app_services.secrets :
-    secret.name
-    if ! contains(local.secret_names_exclude, secret.name)
-  ]
 }
