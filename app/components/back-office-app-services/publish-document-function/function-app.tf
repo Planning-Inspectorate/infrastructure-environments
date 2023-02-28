bdc2637c3869834d67f6c0e8a3d5d71005ee29ff
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-02-28 15:41:03 +0000

    feat: adds new BO function to publish documents

---
+++ app/components/back-office-app-services/publish-document-function/function-app.tf
@@ -0,0 +1,25 @@
+module "publish_document_functions" {
+  source = "../../../modules/node-function-app"
+
+  action_group_low_id                      = var.action_group_low_id
+  app_name                                 = "doc-publisher"
+  app_service_plan_id                      = var.app_service_plan_id
+  function_apps_storage_account            = var.function_apps_storage_account
+  function_apps_storage_account_access_key = var.function_apps_storage_account_access_key
+  integration_subnet_id                    = var.integration_subnet_id
+  location                                 = var.location
+  log_analytics_workspace_id               = var.log_analytics_workspace_id
+  monitoring_alerts_enabled                = var.monitoring_alerts_enabled
+  outbound_vnet_connectivity               = true
+  resource_group_name                      = var.resource_group_name
+  resource_suffix                          = var.resource_suffix
+  service_name                             = "doc-publisher"
+  use_app_insights                         = true
+
+  app_settings = {
+    DOCUMENT_STORAGE_CONNECTION_STRING = var.back_office_document_storage_connection_string
+    API_HOST                           = var.back_office_api_host
+  }
+
+  tags = var.tags
+}
---
+++ app/components/back-office-app-services/publish-document-function/iam.tf
@@ -0,0 +1,11 @@
+resource "azurerm_role_assignment" "function_blob_data_owner" {
+  scope                = var.document_storage_back_office_documents_id
+  role_definition_name = "Storage Blob Data Owner"
+  principal_id         = module.publish_document_functions.principal_id
+}
+
+resource "azurerm_role_assignment" "function_queue_data_contributor" {
+  scope                = var.document_storage_back_office_documents_id
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = module.publish_document_functions.principal_id
+}
