2e6334b0d11c47871aae05d5fc3710d2a61cf42a
nasir-rahman9 <nasirrahman@live.co.uk>
2023-03-13 16:02:15 +0000

    hash out sql kvs to resolve issue

--- app/stacks/uk-west/applications-service/secrets.tf
+++ app/stacks/uk-west/applications-service/secrets.tf
@@ -25,35 +25,35 @@ resource "azurerm_key_vault_secret" "redis_cache_connection_string" {
   value        = azurerm_redis_cache.applications_service_redis_cache.primary_connection_string
 }
 
-resource "azurerm_key_vault_secret" "applications_sql_server_password" {
-  #checkov:skip=CKV_AZURE_41
+# resource "azurerm_key_vault_secret" "applications_sql_server_password" {
+#   #checkov:skip=CKV_AZURE_41
 
-  content_type = "text/plain"
-  key_vault_id = var.key_vault_id
-  name         = "applications-service-sql-server-password"
-  value        = azurerm_mssql_server.applications_sql_server.administrator_login_password
+#   content_type = "text/plain"
+#   key_vault_id = var.key_vault_id
+#   name         = "applications-service-sql-server-password"
+#   value        = azurerm_mssql_server.applications_sql_server.administrator_login_password
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-resource "azurerm_key_vault_secret" "applications_sql_server_username" {
-  #checkov:skip=CKV_AZURE_41
+# resource "azurerm_key_vault_secret" "applications_sql_server_username" {
+#   #checkov:skip=CKV_AZURE_41
 
-  content_type = "text/plain"
-  key_vault_id = var.key_vault_id
-  name         = "applications-service-sql-server-username"
-  value        = azurerm_mssql_server.applications_sql_server.administrator_login
+#   content_type = "text/plain"
+#   key_vault_id = var.key_vault_id
+#   name         = "applications-service-sql-server-username"
+#   value        = azurerm_mssql_server.applications_sql_server.administrator_login
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-resource "azurerm_key_vault_secret" "applications_sql_server_connection_string" {
-  #checkov:skip=CKV_AZURE_41
+# resource "azurerm_key_vault_secret" "applications_sql_server_connection_string" {
+#   #checkov:skip=CKV_AZURE_41
 
-  content_type = "text/plain"
-  key_vault_id = var.key_vault_id
-  name         = "applications-service-sql-server-connection-string"
-  value        = local.sql_connection_string
+#   content_type = "text/plain"
+#   key_vault_id = var.key_vault_id
+#   name         = "applications-service-sql-server-connection-string"
+#   value        = local.sql_connection_string
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
