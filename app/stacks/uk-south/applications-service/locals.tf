82c76d9f5c93d1287cfe24cb6a91761903d997a0
James P (K+C) <james.paterson@kinandcarta.com>
2023-04-03 09:42:57 +0100

    fix(applications-service): function app config fixes (#241)

--- app/stacks/uk-south/applications-service/locals.tf
+++ app/stacks/uk-south/applications-service/locals.tf
@@ -1,8 +1,19 @@
 locals {
-  service_name          = "applications-service"
-  resource_suffix       = "${var.environment}-${module.azure_region_uks.location_short}-${var.instance}"
-  sql_server_username   = "pins-sql-${local.service_name}-${local.resource_suffix}-admin-${random_id.username_suffix.id}"
-  sql_connection_string = "sqlserver://${azurerm_mssql_server.applications_sql_server.fully_qualified_domain_name};database=${azurerm_mssql_database.applications_sql_db.name};user=${azurerm_mssql_server.applications_sql_server.administrator_login};password=${azurerm_mssql_server.applications_sql_server.administrator_login_password};trustServerCertificate=true"
+  service_name        = "applications-service"
+  resource_suffix     = "${var.environment}-${module.azure_region_uks.location_short}-${var.instance}"
+  sql_server_username = "pins-sql-${local.service_name}-${local.resource_suffix}-admin-${random_id.username_suffix.id}"
+  sql_connection_string = join(
+    ";",
+    [
+      "Server=tcp:${azurerm_mssql_server.applications_sql_server.fully_qualified_domain_name},1433",
+      "Initial Catalog=${azurerm_mssql_database.applications_sql_db.name}",
+      "User ID=${azurerm_mssql_server.applications_sql_server.administrator_login}",
+      "Password=${azurerm_mssql_server.applications_sql_server.administrator_login_password}",
+      "Encrypt=True",
+      "TrustServerCertificate=False",
+      "Connection Timeout=30"
+    ]
+  )
 
   tags = merge(
     var.common_tags,
