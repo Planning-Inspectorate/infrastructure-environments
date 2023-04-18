475de51c77868355ebeea5b3890e7dcc6b3f1c2c
nasir-rahman9 <nasirrahman@live.co.uk>
2023-04-18 14:05:56 +0100

    updated staging name

--- app/modules/node-app-service/main.tf
+++ app/modules/node-app-service/main.tf
@@ -163,7 +163,7 @@ resource "azurerm_private_endpoint" "private_endpoint_staging" {
   private_service_connection {
     name                           = "privateendpointconnection-staging"
     private_connection_resource_id = azurerm_linux_web_app_slot.staging.id
-    subresource_names              = ["sites"]
+    subresource_names              = ["sites-staging"]
     is_manual_connection           = false
   }
 
