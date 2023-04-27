8c3dc6190d32e33d254dbeca7dd0e2abd7332579
Mayya Bondarevskaya <mayya.bondarevskaya@kinandcarta.com>
2023-04-27 15:38:08 +0100

    fix: adds service bus namespace name arg (BOAS-834)

--- app/components/back-office-app-services/publish-document-function/variables.tf
+++ app/components/back-office-app-services/publish-document-function/variables.tf
@@ -65,7 +65,6 @@ variable "app_service_plan_id" {
 }
 
 variable "service_bus_namespace_name" {
-  default     = ""
   description = "The name of the Back Office service bus namespace"
   type        = string
 }
