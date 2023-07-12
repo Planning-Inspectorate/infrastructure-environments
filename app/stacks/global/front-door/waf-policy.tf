5ca16681576157ed58cb3057bce498d74e554351
nasir-rahman9 <nasirrahman@live.co.uk>
2023-07-12 14:59:11 +0100

    updated deprecated resource for waf policy

--- app/stacks/global/front-door/waf-policy.tf
+++ app/stacks/global/front-door/waf-policy.tf
@@ -1,6 +1,7 @@
-resource "azurerm_frontdoor_firewall_policy" "default" {
+resource "azurerm_cdn_frontdoor_firewall_policy" "default" {
   name                              = replace("pinswaf${local.service_name}${local.resource_suffix}", "-", "")
   resource_group_name               = azurerm_resource_group.frontdoor.name
+  sku_name                          = azurerm_cdn_frontdoor_profile.common.sku_name
   enabled                           = true
   mode                              = var.front_door_waf_mode
   custom_block_response_status_code = 429
