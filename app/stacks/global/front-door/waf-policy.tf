52d01726d0295218e7f1f0b102f314612fc030c4
nasir-rahman9 <nasirrahman@live.co.uk>
2023-07-13 10:36:39 +0100

    change resource

--- app/stacks/global/front-door/waf-policy.tf
+++ app/stacks/global/front-door/waf-policy.tf
@@ -1,4 +1,4 @@
-resource "azurerm_cdn_frontdoor_firewall_policy" "default" {
+resource "azurerm_frontdoor_firewall_policy" "default" {
   name                              = replace("pinswaf${local.service_name}${local.resource_suffix}", "-", "")
   resource_group_name               = azurerm_resource_group.frontdoor.name
   enabled                           = true
