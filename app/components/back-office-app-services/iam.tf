840bf03888db474500e28216d909892f2a7e6e4e
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2023-07-18 17:06:55 +0100

    fix(appeals/web): document storage permissions

--- app/components/back-office-app-services/iam.tf
+++ app/components/back-office-app-services/iam.tf
@@ -30,7 +30,7 @@ resource "azurerm_role_assignment" "back_office_app_send_service_bus_access" {
 }
 
 # As above, assume appeals will choose its own topics to publish to on the same service bus instance
-resource "azurerm_role_assignment" "back_office_app_send_service_bus_access" {
+resource "azurerm_role_assignment" "back_office_appeals_send_service_bus_access" {
   scope                = var.service_bus_namespace_id
   role_definition_name = "Azure Service Bus Data Sender"
   principal_id         = module.app_service["back_office_appeals-api"].principal_id
