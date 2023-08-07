6d8b5240f36142ecf189ff23c09c4accb74bbace
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-08-07 13:16:35 +0100

    Define variables for new Azure function

---
+++ app/components/back-office-app-services/deadline-submissions-function/variables.tf
@@ -0,0 +1,60 @@
+variable "action_group_low_id" {
+  description = "The ID of the Azure Monitor action group for low priority alerts"
+  type        = string
+}
+
+variable "app_service_plan_id" {
+  description = "The id of the app service plan"
+  type        = string
+}
+
+variable "function_apps_storage_account" {
+  description = "Function Storage name"
+  type        = string
+}
+
+variable "function_apps_storage_account_access_key" {
+  description = "Function Storage Primary Access Key"
+  type        = string
+}
+
+variable "integration_subnet_id" {
+  description = "The id of the vnet integration subnet the app service is linked to for egress traffic"
+  type        = string
+}
+
+variable "location" {
+  description = "The location the App Services are deployed to in slug format e.g. 'uk-south'"
+  type        = string
+}
+
+variable "log_analytics_workspace_id" {
+  description = "The ID of the Azure Monitor Log Analytics Workspace"
+  type        = string
+}
+
+variable "monitoring_alerts_enabled" {
+  default     = false
+  description = "Indicates whether Azure Monitor alerts are enabled for App Service"
+  type        = bool
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group that will contain the App Services"
+  type        = string
+}
+
+variable "resource_suffix" {
+  description = "The suffix for resource naming"
+  type        = string
+}
+
+variable "service_bus_namespace_name" {
+  description = "The name of the Back Office service bus namespace"
+  type        = string
+}
+
+variable "servicebus_topic_deadline_submission_subscription_id" {
+  description = "Service Bus Topic register-nsip-subscription id"
+  type        = string
+}
