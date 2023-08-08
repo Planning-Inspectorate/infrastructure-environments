bb38a6da7f73ea108a3535f22635d21b1fe1638a
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2023-08-08 15:26:16 +0100

    changing from source calls protocol

--- app/components/back-office-app-services/publish-document-function/function-app.tf
+++ app/components/back-office-app-services/publish-document-function/function-app.tf
@@ -1,5 +1,5 @@
 module "publish_document_functions" {
-  source = "git@github.com:Planning-Inspectorate/infrastructure-modules.git/modules/node-function-app?ref=1.1"
+  source = "https://github.com/Planning-Inspectorate/infrastructure-modules.git/modules/node-function-app"
 
   action_group_low_id                      = var.action_group_low_id
   app_name                                 = "doc-publisher"
