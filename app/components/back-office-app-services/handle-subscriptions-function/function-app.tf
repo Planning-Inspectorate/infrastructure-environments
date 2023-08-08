8e37cd2bbfd9a61a3a53b40243ab1515d3765489
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2023-08-08 14:06:23 +0100

    changing from ssh to https source calls

--- app/components/back-office-app-services/handle-subscriptions-function/function-app.tf
+++ app/components/back-office-app-services/handle-subscriptions-function/function-app.tf
@@ -1,5 +1,5 @@
 module "handle_subscriptions_function" {
-  source = "git@github.com:Planning-Inspectorate/infrastructure-environments.git/modules/node-function-app?ref=1.1"
+  source = "https://github.com/Planning-Inspectorate/infrastructure-modules.git/modules/node-function-app?ref=1.1"
 
   action_group_low_id                      = var.action_group_low_id
   app_name                                 = "handle-subs"
