d356754dd70fd2d3285844a769cb6aa0679ec734
nasir-rahman9 <nasir.rahman@planninginspectorate.gov.uk>
2023-08-09 15:37:16 +0100

    updated my name for cdn profile and endpoint

--- app/stacks/global/front-door/front-door-premium.tf
+++ app/stacks/global/front-door/front-door-premium.tf
@@ -130,7 +130,7 @@ resource "azurerm_cdn_frontdoor_rule" "book_reference_file" {
 }
 
 resource "azurerm_cdn_profile" "back_office" {
-  name                = local.back_office_frontend
+  name                = local.back_office_frontend.name
   location            = var.cdn_location
   resource_group_name = azurerm_resource_group.frontdoor.name
   sku                 = "Standard_Verizon"
@@ -145,7 +145,7 @@ resource "azurerm_cdn_profile" "back_office" {
 }
 
 resource "azurerm_cdn_endpoint" "back_office" {
-  name                = local.back_office_frontend
+  name                = local.back_office_frontend.name
   profile_name        = azurerm_cdn_profile.back_office.name
   location            = var.cdn_location
   resource_group_name = azurerm_resource_group.frontdoor.name
