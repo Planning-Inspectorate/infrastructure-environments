4a90c4e13efccfec00d85695e40701931c1a2acb
nasir-rahman9 <nasir.rahman@planninginspectorate.gov.uk>
2023-08-09 09:40:18 +0100

    hashed out custom domain association

--- app/stacks/global/front-door/front-door-premium.tf
+++ app/stacks/global/front-door/front-door-premium.tf
@@ -77,10 +77,10 @@ resource "azurerm_cdn_frontdoor_custom_domain" "common" {
   }
 }
 
-resource "azurerm_cdn_frontdoor_custom_domain_association" "common" {
-  cdn_frontdoor_custom_domain_id = azurerm_cdn_frontdoor_custom_domain.common.id
-  cdn_frontdoor_route_ids        = [azurerm_cdn_frontdoor_route.back_office_appeals_frontend.id]
-}
+# resource "azurerm_cdn_frontdoor_custom_domain_association" "common" {
+#   cdn_frontdoor_custom_domain_id = azurerm_cdn_frontdoor_custom_domain.common.id
+#   cdn_frontdoor_route_ids        = [azurerm_cdn_frontdoor_route.back_office_appeals_frontend.id]
+# }
 
 resource "azurerm_cdn_frontdoor_rule_set" "search_indexing" {
   name                     = "searchindex"
