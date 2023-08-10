139524c86f78d4dd342183c0fcbd08ebb6c8d08e
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2023-08-10 10:44:40 +0100

    change git url

--- app/components/back-office-app-services/publish-document-function/function-app.tf
+++ app/components/back-office-app-services/publish-document-function/function-app.tf
@@ -1,5 +1,5 @@
 module "publish_document_functions" {
-  source = "git::ssh://Planning-Inspectorate/infrastructure-modules/modules/node-function-app.git?ref=1.1"
+  source = "git@ssh.dev.azure.com:v3/Planning-Inspectorate/infrastructure-modules/modules/node-function-app.git?ref=1.1"
 
   action_group_low_id                      = var.action_group_low_id
   app_name                                 = "doc-publisher"
