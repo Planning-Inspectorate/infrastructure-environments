fbfbf1824827e3bb3296b9627bff903e57a194fd
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2023-08-10 08:51:25 +0100

    formatting syntax

--- app/components/back-office-app-services/odw-subscriber-function/function-app.tf
+++ app/components/back-office-app-services/odw-subscriber-function/function-app.tf
@@ -1,5 +1,5 @@
 module "odw_subscriber_function" {
-  source = "git@github.com:Planning-Inspectorate/infrastructure-modules/modules/node-function-app.git?ref=1.1"
+  source = "git::ssh://Planning-Inspectorate/infrastructure-modules/modules/node-function-app.git?ref=1.1"
 
   action_group_low_id                      = var.action_group_low_id
   app_name                                 = "odw-subscriber"
