d942814f866dc9b53e2f61e52bc47adc8382929a
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2023-08-10 15:17:05 +0100

    change git url

--- app/components/back-office-app-services/app-service.tf
+++ app/components/back-office-app-services/app-service.tf
@@ -2,7 +2,7 @@ module "app_service" {
   for_each = local.app_services
 
   source = "git::ssh://github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-app-service?ref=1.1"
-  
+
   action_group_low_id                   = var.action_group_low_id
   app_name                              = each.value["app_name"]
   app_service_plan_id                   = var.app_service_plan_id
