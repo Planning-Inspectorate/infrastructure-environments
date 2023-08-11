44474550f3fd05eec06f46174718185e121a532d
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2023-08-11 14:14:01 +0100

    change to https call

--- app/components/back-office-app-services/odw-subscriber-function/function-app.tf
+++ app/components/back-office-app-services/odw-subscriber-function/function-app.tf
@@ -1,6 +1,6 @@
 module "odw_subscriber_function" {
 
-  source = "github.com/Planning-Inspectorate/infrastructure-modules//node-function-app.git?ref=1.3"
+  source = "git::https://github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-function-app.git?ref=1.3"
 
   action_group_low_id                      = var.action_group_low_id
   app_name                                 = "odw-subscriber"
