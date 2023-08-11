f3708838c705fadf004b3130122855e74748e9f9
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2023-08-11 14:40:56 +0100

    change to https call

--- app/components/back-office-app-services/publish-document-function/function-app.tf
+++ app/components/back-office-app-services/publish-document-function/function-app.tf
@@ -1,6 +1,6 @@
 module "publish_document_functions" {
 
-  source = "git::https://github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-function-app.git?ref=1.3"
+  source = "git::https://github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-function-app?ref=1.3"
 
   action_group_low_id                      = var.action_group_low_id
   app_name                                 = "doc-publisher"
