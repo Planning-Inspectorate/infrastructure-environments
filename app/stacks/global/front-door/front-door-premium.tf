f0837e7e1ca546541bed2d022190e8062b8490ec
nasir-rahman9 <nasir.rahman@planninginspectorate.gov.uk>
2023-08-15 10:17:48 +0100

    added front door premium configs

--- app/stacks/global/front-door/front-door-premium.tf
+++ app/stacks/global/front-door/front-door-premium.tf
@@ -1,229 +1,229 @@
-# resource "azurerm_cdn_frontdoor_profile" "common" {
-#   name                = "pins-fdp-${local.service_name}-${local.resource_suffix}"
-#   resource_group_name = azurerm_resource_group.frontdoor.name
-#   sku_name            = var.front_door_sku_name
+resource "azurerm_cdn_frontdoor_profile" "common" {
+  name                = "pins-fdp-${local.service_name}-${local.resource_suffix}"
+  resource_group_name = azurerm_resource_group.frontdoor.name
+  sku_name            = var.front_door_sku_name
+}
+
+resource "azurerm_cdn_frontdoor_endpoint" "common" {
+  name                     = "pins-fdp-${local.service_name}-${local.resource_suffix}"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
+
+  tags = local.tags
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "common" {
+  name                     = "pins-fdp-${local.service_name}-${local.resource_suffix}"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
+  session_affinity_enabled = false
+
+  load_balancing {
+    sample_size                        = 4
+    successful_samples_required        = 2
+    additional_latency_in_milliseconds = 0
+  }
+
+  health_probe {
+    path                = "/"
+    protocol            = "Http"
+    request_type        = "GET"
+    interval_in_seconds = 120
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin" "common" {
+  name                          = "pins-fdp-${local.service_name}-${local.resource_suffix}"
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.common.id
+
+  enabled                        = true
+  host_name                      = "www.gov.uk"
+  http_port                      = 80
+  https_port                     = 443
+  origin_host_header             = "www.gov.uk"
+  priority                       = 5
+  weight                         = 100
+  certificate_name_check_enabled = true
+}
+
+resource "azurerm_cdn_frontdoor_route" "common" {
+  name                          = "pins-fdp-${local.service_name}-${local.resource_suffix}"
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.common.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.common.id
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.common.id]
+
+  supported_protocols    = ["Http", "Https"]
+  patterns_to_match      = ["/*"]
+  forwarding_protocol    = "MatchRequest"
+  link_to_default_domain = true
+  https_redirect_enabled = true
+
+  cdn_frontdoor_origin_path = "/government/organisations/planning-inspectorate"
+}
+
+resource "azurerm_cdn_frontdoor_endpoint" "back_office_applications_service" {
+  name                     = local.back_office_frontend.frontend_name
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
+
+  tags = local.tags
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "back_office_applications_service" {
+  name                     = local.back_office_frontend.frontend_name
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
+  session_affinity_enabled = false
+
+  load_balancing {
+    sample_size                        = 4
+    successful_samples_required        = 2
+    additional_latency_in_milliseconds = 0
+  }
+
+  health_probe {
+    path                = "/"
+    protocol            = "Http"
+    request_type        = "GET"
+    interval_in_seconds = 120
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin" "back_office_applications_service" {
+  name                          = local.back_office_frontend.frontend_name
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.back_office_applications_service.id
+
+  enabled                        = true
+  host_name                      = local.back_office_frontend.frontend_endpoint
+  http_port                      = 80
+  https_port                     = 443
+  origin_host_header             = local.back_office_frontend.frontend_endpoint
+  priority                       = 5
+  weight                         = 100
+  certificate_name_check_enabled = true
+}
+
+resource "azurerm_cdn_frontdoor_route" "back_office_applications_service" {
+  name                          = local.back_office_frontend.frontend_name
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.back_office_applications_service.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.back_office_applications_service.id
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.back_office_applications_service.id]
+
+  supported_protocols    = ["Http", "Https"]
+  patterns_to_match      = ["/*"]
+  forwarding_protocol    = "MatchRequest"
+  link_to_default_domain = true
+  https_redirect_enabled = true
+}
+
+resource "azurerm_cdn_frontdoor_endpoint" "back_office_appeals_service" {
+  name                     = local.back_office_frontend.frontend_name
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
+
+  tags = local.tags
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "back_office_appeals_service" {
+  name                     = local.back_office_appeals_frontend.frontend_name
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
+  session_affinity_enabled = false
+
+  load_balancing {
+    sample_size                        = 4
+    successful_samples_required        = 2
+    additional_latency_in_milliseconds = 0
+  }
+
+  health_probe {
+    path                = "/"
+    protocol            = "Http"
+    request_type        = "GET"
+    interval_in_seconds = 120
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin" "back_office_appeals_service" {
+  name                          = local.back_office_appeals_frontend.frontend_name
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.back_office_appeals_service.id
+
+  enabled                        = true
+  host_name                      = local.back_office_appeals_frontend.frontend_endpoint
+  http_port                      = 80
+  https_port                     = 443
+  origin_host_header             = local.back_office_appeals_frontend.frontend_endpoint
+  priority                       = 5
+  weight                         = 100
+  certificate_name_check_enabled = true
+}
+
+resource "azurerm_cdn_frontdoor_route" "back_office_appeals_service" {
+  name                          = local.back_office_appeals_frontend.frontend_name
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.back_office_appeals_service.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.back_office_appeals_service.id
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.back_office_appeals_service.id]
+
+  supported_protocols    = ["Http", "Https"]
+  patterns_to_match      = ["/*"]
+  forwarding_protocol    = "MatchRequest"
+  link_to_default_domain = true
+  https_redirect_enabled = true
+}
+
+resource "azurerm_cdn_frontdoor_custom_domain" "common" {
+  name                     = "pins-fdp-${local.service_name}-${local.resource_suffix}"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
+  host_name                = "www.gov.uk"
+
+  tls {
+    certificate_type    = "ManagedCertificate"
+    minimum_tls_version = "TLS12"
+  }
+}
+
+# resource "azurerm_cdn_frontdoor_custom_domain_association" "common" {
+#   cdn_frontdoor_custom_domain_id = azurerm_cdn_frontdoor_custom_domain.common.id
+#   cdn_frontdoor_route_ids        = [azurerm_cdn_frontdoor_route.back_office_appeals_frontend.id]
 # }
 
-# resource "azurerm_cdn_frontdoor_endpoint" "common" {
-#   name                     = "pins-fdp-${local.service_name}-${local.resource_suffix}"
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
+resource "azurerm_cdn_frontdoor_rule_set" "search_indexing" {
+  name                     = "searchindex"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
+}
 
-#   tags = local.tags
-# }
+resource "azurerm_cdn_frontdoor_rule" "addrobotstagheader" {
+  depends_on = [azurerm_cdn_frontdoor_origin_group.common, azurerm_cdn_frontdoor_origin.common]
 
-# resource "azurerm_cdn_frontdoor_origin_group" "common" {
-#   name                     = "pins-fdp-${local.service_name}-${local.resource_suffix}"
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
-#   session_affinity_enabled = false
+  name                      = "addrobotstagheader"
+  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.search_indexing.id
+  order                     = 1
+  behavior_on_match         = "Continue"
 
-#   load_balancing {
-#     sample_size                        = 4
-#     successful_samples_required        = 2
-#     additional_latency_in_milliseconds = 0
-#   }
+  actions {
+    response_header_action {
+      header_action = "Append"
+      header_name   = "X-Robots-Tag"
+      value         = "noindex,nofollow"
+    }
+  }
+}
 
-#   health_probe {
-#     path                = "/"
-#     protocol            = "Http"
-#     request_type        = "GET"
-#     interval_in_seconds = 120
-#   }
-# }
+resource "azurerm_cdn_frontdoor_rule" "book_reference_file" {
+  depends_on = [azurerm_cdn_frontdoor_origin_group.common, azurerm_cdn_frontdoor_origin.common]
 
-# resource "azurerm_cdn_frontdoor_origin" "common" {
-#   name                          = "pins-fdp-${local.service_name}-${local.resource_suffix}"
-#   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.common.id
+  name                      = "BookReferenceFileRobotsTag"
+  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.search_indexing.id
+  order                     = 2
+  behavior_on_match         = "Continue"
 
-#   enabled                        = true
-#   host_name                      = "www.gov.uk"
-#   http_port                      = 80
-#   https_port                     = 443
-#   origin_host_header             = "www.gov.uk"
-#   priority                       = 5
-#   weight                         = 100
-#   certificate_name_check_enabled = true
-# }
+  conditions {
+    url_filename_condition {
+      operator     = "Contains"
+      match_values = ["book", "reference"]
+      transforms   = ["Lowercase"]
+    }
+  }
 
-# resource "azurerm_cdn_frontdoor_route" "common" {
-#   name                          = "pins-fdp-${local.service_name}-${local.resource_suffix}"
-#   cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.common.id
-#   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.common.id
-#   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.common.id]
-
-#   supported_protocols    = ["Http", "Https"]
-#   patterns_to_match      = ["/*"]
-#   forwarding_protocol    = "MatchRequest"
-#   link_to_default_domain = true
-#   https_redirect_enabled = true
-
-#   cdn_frontdoor_origin_path = "/government/organisations/planning-inspectorate"
-# }
-
-# resource "azurerm_cdn_frontdoor_endpoint" "back_office_applications_service" {
-#   name                     = local.back_office_frontend.frontend_name
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
-
-#   tags = local.tags
-# }
-
-# resource "azurerm_cdn_frontdoor_origin_group" "back_office_applications_service" {
-#   name                     = local.back_office_frontend.frontend_name
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
-#   session_affinity_enabled = false
-
-#   load_balancing {
-#     sample_size                        = 4
-#     successful_samples_required        = 2
-#     additional_latency_in_milliseconds = 0
-#   }
-
-#   health_probe {
-#     path                = "/"
-#     protocol            = "Http"
-#     request_type        = "GET"
-#     interval_in_seconds = 120
-#   }
-# }
-
-# resource "azurerm_cdn_frontdoor_origin" "back_office_applications_service" {
-#   name                          = local.back_office_frontend.frontend_name
-#   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.back_office_applications_service.id
-
-#   enabled                        = true
-#   host_name                      = local.back_office_frontend.frontend_endpoint
-#   http_port                      = 80
-#   https_port                     = 443
-#   origin_host_header             = local.back_office_frontend.frontend_endpoint
-#   priority                       = 5
-#   weight                         = 100
-#   certificate_name_check_enabled = true
-# }
-
-# resource "azurerm_cdn_frontdoor_route" "back_office_applications_service" {
-#   name                          = local.back_office_frontend.frontend_name
-#   cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.back_office_applications_service.id
-#   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.back_office_applications_service.id
-#   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.back_office_applications_service.id]
-
-#   supported_protocols    = ["Http", "Https"]
-#   patterns_to_match      = ["/*"]
-#   forwarding_protocol    = "MatchRequest"
-#   link_to_default_domain = true
-#   https_redirect_enabled = true
-# }
-
-# resource "azurerm_cdn_frontdoor_endpoint" "back_office_appeals_service" {
-#   name                     = local.back_office_frontend.frontend_name
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
-
-#   tags = local.tags
-# }
-
-# resource "azurerm_cdn_frontdoor_origin_group" "back_office_appeals_service" {
-#   name                     = local.back_office_appeals_frontend.frontend_name
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
-#   session_affinity_enabled = false
-
-#   load_balancing {
-#     sample_size                        = 4
-#     successful_samples_required        = 2
-#     additional_latency_in_milliseconds = 0
-#   }
-
-#   health_probe {
-#     path                = "/"
-#     protocol            = "Http"
-#     request_type        = "GET"
-#     interval_in_seconds = 120
-#   }
-# }
-
-# resource "azurerm_cdn_frontdoor_origin" "back_office_appeals_service" {
-#   name                          = local.back_office_appeals_frontend.frontend_name
-#   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.back_office_appeals_service.id
-
-#   enabled                        = true
-#   host_name                      = local.back_office_appeals_frontend.frontend_endpoint
-#   http_port                      = 80
-#   https_port                     = 443
-#   origin_host_header             = local.back_office_appeals_frontend.frontend_endpoint
-#   priority                       = 5
-#   weight                         = 100
-#   certificate_name_check_enabled = true
-# }
-
-# resource "azurerm_cdn_frontdoor_route" "back_office_appeals_service" {
-#   name                          = local.back_office_appeals_frontend.frontend_name
-#   cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.back_office_appeals_service.id
-#   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.back_office_appeals_service.id
-#   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.back_office_appeals_service.id]
-
-#   supported_protocols    = ["Http", "Https"]
-#   patterns_to_match      = ["/*"]
-#   forwarding_protocol    = "MatchRequest"
-#   link_to_default_domain = true
-#   https_redirect_enabled = true
-# }
-
-# resource "azurerm_cdn_frontdoor_custom_domain" "common" {
-#   name                     = "pins-fdp-${local.service_name}-${local.resource_suffix}"
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
-#   host_name                = "www.gov.uk"
-
-#   tls {
-#     certificate_type    = "ManagedCertificate"
-#     minimum_tls_version = "TLS12"
-#   }
-# }
-
-# # resource "azurerm_cdn_frontdoor_custom_domain_association" "common" {
-# #   cdn_frontdoor_custom_domain_id = azurerm_cdn_frontdoor_custom_domain.common.id
-# #   cdn_frontdoor_route_ids        = [azurerm_cdn_frontdoor_route.back_office_appeals_frontend.id]
-# # }
-
-# resource "azurerm_cdn_frontdoor_rule_set" "search_indexing" {
-#   name                     = "searchindex"
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.common.id
-# }
-
-# resource "azurerm_cdn_frontdoor_rule" "addrobotstagheader" {
-#   depends_on = [azurerm_cdn_frontdoor_origin_group.common, azurerm_cdn_frontdoor_origin.common]
-
-#   name                      = "addrobotstagheader"
-#   cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.search_indexing.id
-#   order                     = 1
-#   behavior_on_match         = "Continue"
-
-#   actions {
-#     response_header_action {
-#       header_action = "Append"
-#       header_name   = "X-Robots-Tag"
-#       value         = "noindex,nofollow"
-#     }
-#   }
-# }
-
-# resource "azurerm_cdn_frontdoor_rule" "book_reference_file" {
-#   depends_on = [azurerm_cdn_frontdoor_origin_group.common, azurerm_cdn_frontdoor_origin.common]
-
-#   name                      = "BookReferenceFileRobotsTag"
-#   cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.search_indexing.id
-#   order                     = 2
-#   behavior_on_match         = "Continue"
-
-#   conditions {
-#     url_filename_condition {
-#       operator     = "Contains"
-#       match_values = ["book", "reference"]
-#       transforms   = ["Lowercase"]
-#     }
-#   }
-
-#   actions {
-#     response_header_action {
-#       header_action = "Append"
-#       header_name   = "X-Robots-Tag"
-#       value         = "noindex,nofollow"
-#     }
-#   }
-# }
+  actions {
+    response_header_action {
+      header_action = "Append"
+      header_name   = "X-Robots-Tag"
+      value         = "noindex,nofollow"
+    }
+  }
+}
--- app/stacks/global/front-door/waf-policy.tf
+++ app/stacks/global/front-door/waf-policy.tf
@@ -160,147 +160,147 @@ resource "azurerm_frontdoor_firewall_policy" "default" {
   tags = var.common_tags
 }
 
-# resource "azurerm_cdn_frontdoor_firewall_policy" "default" {
-#   name                              = replace("pinscdnwaf${local.service_name}${local.resource_suffix}", "-", "")
-#   resource_group_name               = azurerm_resource_group.frontdoor.name
-#   sku_name                          = azurerm_cdn_frontdoor_profile.common.sku_name
-#   enabled                           = true
-#   mode                              = var.front_door_waf_mode
-#   custom_block_response_status_code = 429
+resource "azurerm_cdn_frontdoor_firewall_policy" "default" {
+  name                              = replace("pinscdnwaf${local.service_name}${local.resource_suffix}", "-", "")
+  resource_group_name               = azurerm_resource_group.frontdoor.name
+  sku_name                          = azurerm_cdn_frontdoor_profile.common.sku_name
+  enabled                           = true
+  mode                              = var.front_door_waf_mode
+  custom_block_response_status_code = 429
 
-#   managed_rule {
-#     type    = "DefaultRuleSet"
-#     version = "1.0"
-#     action  = "Block"
+  managed_rule {
+    type    = "DefaultRuleSet"
+    version = "1.0"
+    action  = "Block"
 
-#     override {
-#       rule_group_name = "RFI"
+    override {
+      rule_group_name = "RFI"
 
-#       rule {
-#         # Possible Remote File Inclusion (RFI) Attack: Off-Domain Reference/Link
-#         rule_id = "931130"
-#         action  = "Block"
+      rule {
+        # Possible Remote File Inclusion (RFI) Attack: Off-Domain Reference/Link
+        rule_id = "931130"
+        action  = "Block"
 
-#         exclusion {
-#           # Exclusion to fix BOAS-153
-#           match_variable = "RequestBodyPostArgNames" # PostParamValue:applicant.website
-#           operator       = "Equals"
-#           selector       = "applicant.website"
-#         }
-#       }
-#     }
+        exclusion {
+          # Exclusion to fix BOAS-153
+          match_variable = "RequestBodyPostArgNames" # PostParamValue:applicant.website
+          operator       = "Equals"
+          selector       = "applicant.website"
+        }
+      }
+    }
 
-#     override {
-#       rule_group_name = "LFI"
+    override {
+      rule_group_name = "LFI"
 
-#       rule {
-#         # Path Traversal Attack (/../)
-#         rule_id = "930100"
-#         action  = "Block"
+      rule {
+        # Path Traversal Attack (/../)
+        rule_id = "930100"
+        action  = "Block"
 
-#         exclusion {
-#           # Exclusion to allow acceptance of cookies
-#           match_variable = "RequestCookieNames" # "CookieValue:cookie_policy"
-#           operator       = "Equals"
-#           selector       = "cookie_policy"
-#         }
-#       }
+        exclusion {
+          # Exclusion to allow acceptance of cookies
+          match_variable = "RequestCookieNames" # "CookieValue:cookie_policy"
+          operator       = "Equals"
+          selector       = "cookie_policy"
+        }
+      }
 
-#       rule {
-#         # Path Traversal Attack (/../)
-#         rule_id = "930110"
-#         action  = "Block"
+      rule {
+        # Path Traversal Attack (/../)
+        rule_id = "930110"
+        action  = "Block"
 
-#         exclusion {
-#           # Exclusion to allow acceptance of cookies
-#           match_variable = "RequestCookieNames" # "CookieValue:cookie_policy"
-#           operator       = "Equals"
-#           selector       = "cookie_policy"
-#         }
-#       }
-#     }
+        exclusion {
+          # Exclusion to allow acceptance of cookies
+          match_variable = "RequestCookieNames" # "CookieValue:cookie_policy"
+          operator       = "Equals"
+          selector       = "cookie_policy"
+        }
+      }
+    }
 
-#     override {
-#       rule_group_name = "SQLI"
+    override {
+      rule_group_name = "SQLI"
 
-#       rule {
-#         # Detects MySQL comment-/space-obfuscated injections and backtick termination
-#         rule_id = "942200"
-#         action  = "Block"
+      rule {
+        # Detects MySQL comment-/space-obfuscated injections and backtick termination
+        rule_id = "942200"
+        action  = "Block"
 
-#         exclusion {
-#           # Exclusion to allow acceptance of cookies
-#           match_variable = "RequestCookieNames" # "CookieValue:cookie_policy"
-#           operator       = "Equals"
-#           selector       = "cookie_policy"
-#         }
-#       }
+        exclusion {
+          # Exclusion to allow acceptance of cookies
+          match_variable = "RequestCookieNames" # "CookieValue:cookie_policy"
+          operator       = "Equals"
+          selector       = "cookie_policy"
+        }
+      }
 
-#       rule {
-#         # Detects basic SQL authentication bypass attempts 2/3
-#         rule_id = "942260"
-#         action  = "Block"
+      rule {
+        # Detects basic SQL authentication bypass attempts 2/3
+        rule_id = "942260"
+        action  = "Block"
 
-#         exclusion {
-#           # Exclusion to allow acceptance of cookies
-#           match_variable = "RequestCookieNames" # "CookieValue:cookie_policy"
-#           operator       = "Equals"
-#           selector       = "cookie_policy"
-#         }
-#       }
+        exclusion {
+          # Exclusion to allow acceptance of cookies
+          match_variable = "RequestCookieNames" # "CookieValue:cookie_policy"
+          operator       = "Equals"
+          selector       = "cookie_policy"
+        }
+      }
 
-#       rule {
-#         # Restricted SQL Character Anomaly Detection (args): # of special characters exceeded (12)
-#         rule_id = "942430"
-#         action  = "Block"
+      rule {
+        # Restricted SQL Character Anomaly Detection (args): # of special characters exceeded (12)
+        rule_id = "942430"
+        action  = "Block"
 
-#         exclusion {
-#           # Exclusion to allow Azure AD authentication redirection
-#           match_variable = "QueryStringArgNames" # "QueryParamValue:code"
-#           operator       = "Equals"
-#           selector       = "code"
-#         }
-#       }
+        exclusion {
+          # Exclusion to allow Azure AD authentication redirection
+          match_variable = "QueryStringArgNames" # "QueryParamValue:code"
+          operator       = "Equals"
+          selector       = "code"
+        }
+      }
 
-#       rule {
-#         # SQL Comment Sequence Detected
-#         rule_id = "942440"
-#         action  = "Block"
+      rule {
+        # SQL Comment Sequence Detected
+        rule_id = "942440"
+        action  = "Block"
 
-#         exclusion {
-#           # Exclusion to allow Azure AD authentication redirection
-#           match_variable = "QueryStringArgNames" # "QueryParamValue:code"
-#           operator       = "Equals"
-#           selector       = "code"
-#         }
-#       }
-#     }
-#   }
+        exclusion {
+          # Exclusion to allow Azure AD authentication redirection
+          match_variable = "QueryStringArgNames" # "QueryParamValue:code"
+          operator       = "Equals"
+          selector       = "code"
+        }
+      }
+    }
+  }
 
-#   custom_rule {
-#     name                           = "RateLimitHttpRequest"
-#     action                         = "Block"
-#     enabled                        = true
-#     priority                       = 100
-#     type                           = "RateLimitRule"
-#     rate_limit_duration_in_minutes = 1
-#     rate_limit_threshold           = 300
+  custom_rule {
+    name                           = "RateLimitHttpRequest"
+    action                         = "Block"
+    enabled                        = true
+    priority                       = 100
+    type                           = "RateLimitRule"
+    rate_limit_duration_in_minutes = 1
+    rate_limit_threshold           = 300
 
-#     match_condition {
-#       match_variable = "RequestMethod"
-#       operator       = "Equal"
-#       match_values = [
-#         "GET",
-#         "POST",
-#         "PUT",
-#         "DELETE",
-#         "COPY",
-#         "MOVE",
-#         "HEAD",
-#         "OPTIONS"
-#       ]
-#     }
-#   }
+    match_condition {
+      match_variable = "RequestMethod"
+      operator       = "Equal"
+      match_values = [
+        "GET",
+        "POST",
+        "PUT",
+        "DELETE",
+        "COPY",
+        "MOVE",
+        "HEAD",
+        "OPTIONS"
+      ]
+    }
+  }
 
-#   tags = var.common_tags
-# }
+  tags = var.common_tags
+}
