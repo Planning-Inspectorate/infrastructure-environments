3a3bc94b51bfce9c0594b3b4796faad6b5475ecf
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-08-25 10:35:49 +0100

    Add missing resource manager ID variable to uk-south stack

--- app/stacks/uk-south/back-office/app-service.tf
+++ app/stacks/uk-south/back-office/app-service.tf
@@ -45,6 +45,7 @@ module "app_services" {
   document_check_function_storage_name                                                = var.document_check_function_storage_name
   document_storage_back_office_document_service_uploads_container_resource_manager_id = var.document_storage_back_office_document_service_uploads_container_resource_manager_id
   document_storage_back_office_published_documents_container_resource_manager_id      = var.document_storage_back_office_published_documents_container_resource_manager_id
+  document_storage_submissions_container_resource_manager_id                          = var.document_storage_submissions_container_resource_manager_id
   document_check_function_storage_primary_access_key                                  = var.document_check_function_storage_primary_access_key
   document_storage_back_office_document_service_uploads_container_name                = var.document_storage_back_office_document_service_uploads_container_name
   servicebus_topic_nsip_documents_id                                                  = var.servicebus_topic_nsip_documents_id
