6253cfd4fff0adc2c2b1e8b14ab791441cc2ab75
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2023-08-30 10:40:00 +0100

    changing module declaration

--- app/stacks/global/front-door/waf-policy.tf
+++ app/stacks/global/front-door/waf-policy.tf
@@ -175,7 +175,7 @@ resource "azurerm_frontdoor_firewall_policy" "default" {
 resource "azurerm_cdn_frontdoor_firewall_policy" "default" {
   name                              = replace("pinscdnwaf${local.service_name}${local.resource_suffix}", "-", "")
   resource_group_name               = azurerm_resource_group.frontdoor.name
-  sku_name                          = azurerm_cdn_frontdoor_profile.common.sku_name
+  sku_name                          = azurerm_cdn_frontdoor_profile.azure_fdp_test.sku_name
   enabled                           = true
   mode                              = var.front_door_waf_mode
   custom_block_response_status_code = 429
