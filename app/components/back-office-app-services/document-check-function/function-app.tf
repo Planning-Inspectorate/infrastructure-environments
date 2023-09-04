08cef919dd69017f859b60498a0c5acdc5c72cfb
Christopher Daly <christopher.daly@kinandcarta.com>
2023-09-04 11:03:37 +0100

    Downgrade to Node 14

--- app/components/back-office-app-services/document-check-function/function-app.tf
+++ app/components/back-office-app-services/document-check-function/function-app.tf
@@ -15,7 +15,7 @@ module "anti_virus_functions" {
   resource_suffix                          = var.resource_suffix
   service_name                             = "doc-check"
   use_app_insights                         = true
-  function_node_version                    = 16
+  function_node_version                    = 14
 
   app_settings = {
     CLAM_AV_HOST              = var.clamav_host
