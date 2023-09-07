20132950714f9a297e512c90308a934fcdf5e27c
Fabio Fabrizio <fabio.fabrizio@kinandcarta.com>
2023-09-07 15:37:08 +0100

    fix(appeals): remove duplicate subscription

--- app/components/back-office-app-services/bo-appeals-hrdata-import-function/iam.tf
+++ app/components/back-office-app-services/bo-appeals-hrdata-import-function/iam.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "service_bus_data_receiver" {
-  scope                = azurerm_servicebus_subscription.employee_topic_subscription.id
+  scope                = azurerm_servicebus_subscription.register_odw_hrdata_subscription.id
   role_definition_name = "Azure Service Bus Data Receiver"
   principal_id         = module.bo_appeals_hrdata_import_function.principal_id
 }
