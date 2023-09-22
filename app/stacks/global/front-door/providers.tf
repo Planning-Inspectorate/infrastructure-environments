7eb134438670a30109ed9519da67bc68330cc0b6
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2023-09-22 11:04:33 +0100

    updating tf versioning

--- app/stacks/global/front-door/providers.tf
+++ app/stacks/global/front-door/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.64.0"
+      version = "~> 3.73.0"
     }
   }
 
-  required_version = ">= 1.1.6, < 3.64.0"
+  required_version = ">= 1.1.6, < 3.73.0"
 }
 
 provider "azurerm" {
