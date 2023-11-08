a42ef921e69e9742f613779cb7e831b09acbe700
Kartikay Jainwal <kartikayjainwal@gmail.com>
2023-11-08 11:24:14 +0000

    chore: added keyvault perms to bo functions

--- app/components/applications-app-services/iam.tf
+++ app/components/applications-app-services/iam.tf
@@ -21,3 +21,16 @@ resource "azurerm_role_assignment" "back_office_deadline_documents_blob_storage_
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = module.app_service["applications_service_api"].principal_id
 }
+
+resource "azurerm_key_vault_access_policy" "bo_subscribers_read_secrets" {
+  count = var.feature_back_office_subscriber_enabled ? 1 : 0
+
+  key_vault_id = var.key_vault_id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.back_office_subscribers[0].principal_id
+
+  certificate_permissions = []
+  key_permissions         = []
+  secret_permissions      = ["Get"]
+  storage_permissions     = []
+}
