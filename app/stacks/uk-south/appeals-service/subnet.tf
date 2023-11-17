93ad4d695f2ca7b1ae7b2d6e28e8187c28e50c76
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-11-17 16:57:00 +0000

    fix(appeals-fo): add storage endpoint for appeals subnet

--- app/stacks/uk-south/appeals-service/subnet.tf
+++ app/stacks/uk-south/appeals-service/subnet.tf
@@ -4,4 +4,5 @@ resource "azurerm_subnet" "appeals_service_ingress" {
   virtual_network_name                      = var.common_vnet_name
   address_prefixes                          = [var.common_vnet_cidr_blocks["appeals_service_endpoints"]]
   private_endpoint_network_policies_enabled = true
+  service_endpoints                         = ["Microsoft.Storage"]
 }
