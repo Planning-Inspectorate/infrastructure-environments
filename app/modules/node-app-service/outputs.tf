29c7de3f9a1ad6679c473e2b43811c604f93d084
nasir-rahman9 <nasir.rahman@planninginspectorate.gov.uk>
2023-11-21 11:54:28 +0000

    Removed redundant modules directory

--- app/modules/node-app-service/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "default_site_hostname" {
-  description = "The Default Hostname associated with the App Service"
-  value       = azurerm_linux_web_app.web_app.default_hostname
-}
-
-output "principal_id" {
-  description = "The ID of the principal associated with the App Service"
-  value       = azurerm_linux_web_app.web_app.identity[0].principal_id
-}
