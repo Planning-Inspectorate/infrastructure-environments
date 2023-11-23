434c7e0d305128c62dd37b9b843a13990858e14a
Christopher Daly <christopher.daly@kinandcarta.com>
2023-11-23 13:28:49 +0000

    Only deploy IAM if ODW Synapse is configured for the env

--- app/components/back-office-app-services/applications-migration-function/iam.tf
+++ app/components/back-office-app-services/applications-migration-function/iam.tf
@@ -13,6 +13,9 @@ resource "azurerm_key_vault_access_policy" "read_secrets" {
 # Annoyingly it doesn't look like you can get a data "azurerm_storage_container" resource in a different Subscription and RG
 # So we need to build the resource ID dynamically
 resource "azurerm_role_assignment" "read_data_lake_storage" {
+  # We're only deploying the synapse migration functionality on Dev right now
+  count = var.odw_subscription_id != "" ? 1 : 0
+
   scope                = "/subscriptions/${var.odw_subscription_id}/resourceGroups/${var.odw_resource_group_name}/providers/Microsoft.Storage/storageAccounts/${var.odw_data_lake_storage_account_name}/blobServices/default/containers/odw_curated"
   role_definition_name = "Storage Blob Data Reader"
   principal_id         = module.applications_migration_function.principal_id
