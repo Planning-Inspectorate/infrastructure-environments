6cfd7158a92c981c6345cdb334b78afa549923d1
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-11-27 09:45:21 +0000

    Add a daily data cap of 10GB to App Insights

--- app/stacks/uk-west/back-office/monitoring.tf
+++ app/stacks/uk-west/back-office/monitoring.tf
@@ -1,9 +1,10 @@
 resource "azurerm_application_insights" "back_office_app_insights" {
-  name                = "pins-ai-${local.service_name}-${local.resource_suffix}-app-insights"
-  location            = azurerm_resource_group.back_office_stack.location
-  resource_group_name = azurerm_resource_group.back_office_stack.name
-  workspace_id        = azurerm_log_analytics_workspace.back_office.id
-  application_type    = "web"
+  name                 = "pins-ai-${local.service_name}-${local.resource_suffix}-app-insights"
+  location             = azurerm_resource_group.back_office_stack.location
+  resource_group_name  = azurerm_resource_group.back_office_stack.name
+  workspace_id         = azurerm_log_analytics_workspace.back_office.id
+  application_type     = "web"
+  daily_data_cap_in_gb = 10
 }
 
 resource "azurerm_log_analytics_workspace" "back_office" {
