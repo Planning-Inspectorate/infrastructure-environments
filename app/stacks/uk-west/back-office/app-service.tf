515ecca5f48e283e70b2addef90b204a05454fe6
Christopher Daly <96062341+chrisdalykinandcarta@users.noreply.github.com>
2023-11-27 18:23:09 +0000

    Provide the ARM ID of the container (#532)

--- app/stacks/uk-west/back-office/app-service.tf
+++ app/stacks/uk-west/back-office/app-service.tf
@@ -46,7 +46,7 @@ module "app_services" {
   common_vnet_id                                                                      = var.common_vnet_id
   bo_appeals_storage_account_name                                                     = azurerm_storage_account.back_office_documents.name
   bo_appeals_document_container_name                                                  = azurerm_storage_container.back_office_appeals_document_container.name
-  bo_appeals_document_container_id                                                    = azurerm_storage_container.back_office_appeals_document_container.id
+  bo_appeals_document_container_id                                                    = azurerm_storage_container.back_office_appeals_document_container.resource_manager_id
   document_check_function_storage_name                                                = azurerm_storage_account.function_storage.name
   document_check_function_storage_primary_access_key                                  = azurerm_storage_account.function_storage.primary_access_key
   document_storage_back_office_document_service_uploads_container_resource_manager_id = azurerm_storage_container.back_office_document_service_uploads_container.resource_manager_id
