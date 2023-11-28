263e77a22f080ee57520822c76afd7c4c3d7aa79
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-11-28 12:21:19 +0000

    fix(back-office): specify resource group for private dns zone

--- app/stacks/uk-south/back-office/data.tf
+++ app/stacks/uk-south/back-office/data.tf
@@ -12,4 +12,6 @@ data "azurerm_private_dns_zone" "database" {
 
 data "azurerm_private_dns_zone" "redis_cache" {
   name = "privatelink.redis.cache.windows.net"
+  # this might get moved one day but exists in the application stack for now
+  resource_group_name = "pins-rg-applications-service-${local.resource_suffix}"
 }
