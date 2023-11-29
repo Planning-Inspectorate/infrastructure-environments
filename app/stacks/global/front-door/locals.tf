bc5a10015d31eec49bc4ba990af3639a3d550095
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-11-29 10:05:03 +0000

    fix: revert name to comment-planning-appeal-gov-uk

--- app/stacks/global/front-door/locals.tf
+++ app/stacks/global/front-door/locals.tf
@@ -115,7 +115,7 @@ locals {
     ]
     infer_backend_host_header = false
     name                      = "CommentPlanningAppeal"
-    frontend_name             = "CommentPlanningAppeal"
+    frontend_name             = "comment-planning-appeal-gov-uk"
     patterns_to_match         = ["/*"]
     ssl_certificate_name      = var.comment_planning_appeal_ssl_certificate_name
   }
