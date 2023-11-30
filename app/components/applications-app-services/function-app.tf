5c81ad0f193276c9051c97a858c61dd1c944e2b6
Kartikay Jainwal <kartikayjainwal@gmail.com>
2023-11-30 10:13:43 +0000

    feat: bo functions project unpublish subscription

--- app/components/applications-app-services/function-app.tf
+++ app/components/applications-app-services/function-app.tf
@@ -59,6 +59,38 @@ resource "azurerm_servicebus_subscription_rule" "nsip_project_topic_subscription
   }
 }
 
+# nsip-project-unpublish
+resource "azurerm_servicebus_subscription" "nsip_project_unpublish_topic_subscription" {
+  count = var.feature_back_office_subscriber_enabled ? 1 : 0
+
+  name               = "applications-nsip-project-unpublish"
+  topic_id           = var.back_office_service_bus_nsip_project_topic_id
+  max_delivery_count = 1
+}
+
+resource "azurerm_role_assignment" "nsip_project_unpublish_service_bus_role" {
+  count = var.feature_back_office_subscriber_enabled ? 1 : 0
+
+  scope                = azurerm_servicebus_subscription.nsip_project_unpublish_topic_subscription[0].id
+  role_definition_name = "Azure Service Bus Data Receiver"
+  principal_id         = module.back_office_subscribers[0].principal_id
+}
+
+resource "azurerm_servicebus_subscription_rule" "nsip_project_unpublish_topic_subscription_rule" {
+  count = var.feature_back_office_subscriber_enabled ? 1 : 0
+
+  name            = "applications-nsip-project-unpublish"
+  subscription_id = azurerm_servicebus_subscription.nsip_project_unpublish_topic_subscription[0].id
+  filter_type     = "CorrelationFilter"
+  correlation_filter {
+    properties = {
+      type = "Unpublish"
+    }
+  }
+}
+
+
+
 # nsip-document
 
 resource "azurerm_servicebus_subscription" "nsip_document_topic_subscription" {
