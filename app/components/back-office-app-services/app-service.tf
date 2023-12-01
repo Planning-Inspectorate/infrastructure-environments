4867b0ae242ed7b57be675f6537f73fe05383baf
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-12-01 13:53:39 +0000

    Pass a health check path to the back-office app service

--- app/components/back-office-app-services/app-service.tf
+++ app/components/back-office-app-services/app-service.tf
@@ -25,6 +25,7 @@ module "app_service" {
   resource_group_name                   = var.resource_group_name
   resource_suffix                       = var.resource_suffix
   service_name                          = var.service_name
+  health_check_path                     = "/health"
 
   tags = var.tags
 
