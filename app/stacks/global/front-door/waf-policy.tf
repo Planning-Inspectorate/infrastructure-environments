16fb407a183cf72192fffeb490633b69d7a0ce78
Philip Philippou <philip@philips-mbp-3.lan>
2023-12-21 13:34:33 +0000

    fix: remove waf block for session cookie sqli

--- app/stacks/global/front-door/waf-policy.tf
+++ app/stacks/global/front-door/waf-policy.tf
@@ -110,6 +110,19 @@ resource "azurerm_frontdoor_firewall_policy" "default" {
           selector       = "code"
         }
       }
+
+      rule {
+        # SQL Hex Encoding Identified
+        rule_id = "942450"
+        action  = "Block"
+
+        exclusion {
+          # Exclusion to allow cookie connect.sid
+          match_variable = "RequestCookieNames" # "CookieValue:connect.sid"
+          operator       = "Equals"
+          selector       = "connect.sid"
+        }
+      }
     }
 
     # Exception for ASB-1692 merged with ASB-1928 - Exclude all rules for this selector.
