4ebf4b4c912c5bba4125c3e56cfec64ae4c76df6
Christopher Daly <christopher.daly@kinandcarta.com>
2024-01-18 09:36:42 +0000

    Fix module reference

--- app/components/back-office-app-services/applications-background-job-functions/event-grid.tf
+++ app/components/back-office-app-services/applications-background-job-functions/event-grid.tf
@@ -3,7 +3,7 @@ resource "azurerm_eventgrid_event_subscription" "malware_scanning_subscription" 
   scope = var.malware_scanning_topic_id
 
   azure_function_endpoint {
-    function_id                       = "${module.malware_detected_functions.app_id}/functions/malware-detected"
+    function_id                       = "${module.applications_background_job_functions.app_id}/functions/malware-detected"
     max_events_per_batch              = 1
     preferred_batch_size_in_kilobytes = 64
   }
