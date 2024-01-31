d07bfe5ff129278fead25fd8b885175090dcc614
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-01-31 14:56:23 +0000

    Revert "fix(appeals-fo): aapd-1237 bring back clamav rest until code released"
    This reverts commit 19f1223ba27f4459962658a6248e87c2a249222c.

--- app/components/appeals-app-services/locals.tf
+++ app/components/appeals-app-services/locals.tf
@@ -19,7 +19,6 @@ locals {
         APPEALS_SERVICE_API_TIMEOUT               = var.api_timeout
         APPEALS_SERVICE_API_URL                   = "https://pins-app-${var.service_name}-appeals-api-${var.resource_suffix}.azurewebsites.net"
         AUTH_BASE_URL                             = "https://pins-app-${var.service_name}-auth-server-${var.resource_suffix}.azurewebsites.net"
-        CLAM_AV_HOST                              = "https://pins-app-${var.service_name}-clamav-api-${var.resource_suffix}.azurewebsites.net"
         CLAMAV_HOST                               = var.clamav_host
         CLAMAV_PORT                               = "3310"
         CLIENT_ID                                 = local.secret_refs["appeals-forms-web-app-client-id"]
@@ -70,7 +69,6 @@ locals {
         APPLICATIONINSIGHTS_CONNECTION_STRING     = local.secret_refs["appeals-app-insights-connection-string"]
         APPEALS_SERVICE_API_TIMEOUT               = var.api_timeout
         APPEALS_SERVICE_API_URL                   = "https://pins-app-${var.service_name}-appeals-api-${var.resource_suffix}.azurewebsites.net"
-        CLAM_AV_HOST                              = "https://pins-app-${var.service_name}-clamav-api-${var.resource_suffix}.azurewebsites.net"
         CLAMAV_HOST                               = var.clamav_host
         CLAMAV_PORT                               = "3310"
         CLIENT_ID                                 = local.secret_refs["appeals-web-comment-client-id"]
@@ -263,22 +261,6 @@ locals {
         SERVER_PORT                             = "3000"
         SERVER_SHOW_ERRORS                      = true
         SERVER_TERMINATION_GRACE_PERIOD_SECONDS = "0"
-      }
-    }
-
-    clam_av_service_api = {
-      app_name                        = "clamav-api"
-      app_service_private_dns_zone_id = var.app_service_private_dns_zone_id
-      endpoint_subnet_id              = var.private_endpoint_enabled ? var.endpoint_subnet_id : null
-      image_name                      = "appeal-planning-decision/clamav-api"
-      integration_subnet_id           = var.integration_subnet_id
-      inbound_vnet_connectivity       = var.private_endpoint_enabled
-      key_vault_access                = true
-      outbound_vnet_connectivity      = true
-
-      app_settings = {
-        APPLICATIONINSIGHTS_CONNECTION_STRING = local.secret_refs["appeals-app-insights-connection-string"]
-        CLAMAV_HOST                           = var.clamav_host
       }
     }
   }
