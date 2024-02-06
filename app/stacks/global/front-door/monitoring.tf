17a2f35c4b1a7eb59a48852ec19dcf3518b85cc7
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2024-02-06 12:58:13 +0000

    feat(appeals-fo): add azure alert for WAF blocks

--- app/stacks/global/front-door/monitoring.tf
+++ app/stacks/global/front-door/monitoring.tf
@@ -18,3 +18,34 @@ resource "azurerm_monitor_diagnostic_setting" "front_door_waf" {
     ]
   }
 }
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "appeals_waf_blocks" {
+  name         = "Appeals WAF blocks"
+  display_name = "appeals WAF blocks"
+  description  = "Triggered when there has been a WAF block for appeals service in the last 2 hours"
+
+  location            = azurerm_resource_group.frontdoor.location
+  resource_group_name = azurerm_resource_group.frontdoor.name
+  scopes              = [azurerm_frontdoor.common.id]
+
+  enabled                          = var.monitoring_alerts_enabled
+  auto_mitigation_enabled          = true
+  workspace_alerts_storage_enabled = false
+
+  evaluation_frequency = "PT2H"
+  window_duration      = "PT2H"
+
+  criteria {
+    query                   = <<-QUERY
+      AzureDiagnostics
+        | where ResourceProvider == "MICROSOFT.NETWORK" and Category == "FrontdoorWebApplicationFirewallLog"
+        | where action_s == "Block"
+        | where host_s == '${var.appeals_service_public_url}'
+      QUERY
+    time_aggregation_method = "Count"
+    threshold               = 1
+    operator                = "GreaterThanOrEqual"
+  }
+
+  severity = 2
+}
