0da1dfb4275fe2f4cd2617e24e7c0b6222c86bd8
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-02-09 11:37:01 +0000

    Create a separate non-admin login for back-office SQL

--- app/stacks/uk-west/back-office/locals.tf
+++ app/stacks/uk-west/back-office/locals.tf
@@ -2,6 +2,9 @@ locals {
   service_name = "back-office"
   # TODO: Let's create database-specific users and passwords instead for connection strings
   # Also, let's store this in Key Vault rather than just spitting it into env variables!
+  sql_server_username_admin = "backofficeadmin_${random_id.username_suffix_admin.id}"
+  sql_server_username_app   = "backofficeapp_${random_id.username_suffix_app.id}"
+
   sql_connection_string_admin = join(
     ";",
     [
@@ -12,9 +15,20 @@ locals {
       "trustServerCertificate=true"
     ]
   )
+  sql_connection_string_app = join(
+    ";",
+    [
+      "sqlserver://${azurerm_mssql_server.back_office.fully_qualified_domain_name}",
+      "database=${azurerm_mssql_database.back_office.name}",
+      "user=${local.sql_server_username_app}",
+      "password=${random_password.back_office_sql_server_password_app.result}",
+      "trustServerCertificate=true"
+    ]
+  )
+
   appeals_sql_connection_string = "sqlserver://${azurerm_mssql_server.back_office.fully_qualified_domain_name};database=${azurerm_mssql_database.back_office_appeals.name};user=${azurerm_mssql_server.back_office.administrator_login};password=${azurerm_mssql_server.back_office.administrator_login_password};trustServerCertificate=true"
-  sql_server_username_admin     = "backofficeadmin_${random_id.username_suffix_admin.id}"
-  resource_suffix               = "${var.environment}-${module.azure_region_ukw.location_short}-${var.instance}"
+
+  resource_suffix = "${var.environment}-${module.azure_region_ukw.location_short}-${var.instance}"
 
   tags = merge(
     var.common_tags,
