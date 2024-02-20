57579b66e795ef98dc546782533a36783c78f0e6
nasir-rahman9 <nasir.rahman@planninginspectorate.gov.uk>
2024-02-20 10:44:55 +0000

    hashed out variables to resolve tflint error

--- app/stacks/global/back-office/variables.tf
+++ app/stacks/global/back-office/variables.tf
@@ -1,20 +1,20 @@
-variable "domain_name" {
-  description = "Public domain name"
-  type        = string
-}
+# variable "domain_name" {
+#   description = "Public domain name"
+#   type        = string
+# }
 
-variable "app_service_domain" {
-  description = "Domain of the primary app service"
-  type        = string
-}
+# variable "app_service_domain" {
+#   description = "Domain of the primary app service"
+#   type        = string
+# }
 
-variable "environment" {
-  description = "Environment Name"
-  type        = string
-}
+# variable "environment" {
+#   description = "Environment Name"
+#   type        = string
+# }
 
-variable "enable_search_indexing_by_default" {
-  description = "A flag to indicate if the environment should enable search indexing for frontends by default"
-  type        = bool
-  default     = false
-}
+# variable "enable_search_indexing_by_default" {
+#   description = "A flag to indicate if the environment should enable search indexing for frontends by default"
+#   type        = bool
+#   default     = false
+# }
