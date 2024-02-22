718fa4369cd63d6c198d8ef1aa373217ebab90d3
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-02-22 13:03:52 +0000

    chore: remove 'low' action group, now unused

--- app/stacks/uk-west/common/action-group.tf
+++ app/stacks/uk-west/common/action-group.tf
@@ -1,23 +1,3 @@
-# note: this is a global resource
-# TODO: deprecate this resource
-resource "azurerm_monitor_action_group" "low" {
-  name                = "pins-ag-low-${local.service_name}-${local.resource_suffix}"
-  resource_group_name = azurerm_resource_group.common_infrastructure.name
-  short_name          = "${var.environment}-low"
-  tags                = local.tags
-
-  dynamic "email_receiver" {
-    for_each = var.alert_recipients["low"]
-    iterator = mapping
-
-    content {
-      name                    = "Send to ${mapping.value}"
-      email_address           = mapping.value
-      use_common_alert_schema = true
-    }
-  }
-}
-
 # note: these are a global resources
 resource "azurerm_monitor_action_group" "all_action_groups" {
   for_each = local.all_action_groups
