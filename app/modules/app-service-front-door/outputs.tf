c1e905a113b8c7a70d63f4ddbbebbd52c5ca99d9
nasir-rahman9 <nasir.rahman@planninginspectorate.gov.uk>
2024-02-29 13:31:42 +0000

    enabled_search index_by default feature

--- app/modules/app-service-front-door/outputs.tf
+++ app/modules/app-service-front-door/outputs.tf
@@ -1,4 +1,6 @@
 output "front_door_profile_id" {
+  count = var.environment == "dev" ? 1 : 0
+
   description = "ID of the Azure Front Door Profile"
   value       = azurerm_cdn_frontdoor_profile.default[count.index].id
 }
--- app/stacks/global/back-office/variables.tf
+++ app/stacks/global/back-office/variables.tf
@@ -23,11 +23,11 @@ variable "environment" {
   type        = string
 }
 
-# variable "enable_search_indexing_by_default" {
-#   description = "A flag to indicate if the environment should enable search indexing for frontends by default"
-#   type        = bool
-#   default     = false
-# }
+variable "enable_search_indexing_by_default" {
+  description = "A flag to indicate if the environment should enable search indexing for frontends by default"
+  type        = bool
+  default     = false
+}
 
 variable "common_log_analytics_workspace_id" {
   description = "The ID for the common Log Analytics Workspace"
@@ -38,6 +38,12 @@ variable "location" {
   description = "The location the App Services are deployed to in slug format e.g. 'uk-south'"
   type        = string
 }
+
+variable "name" {
+  description = "The name of the resource"
+  type        = string
+}
+
 
 variable "resource_group_name" {
   description = "The name of the resource group that will contain the App Services"
