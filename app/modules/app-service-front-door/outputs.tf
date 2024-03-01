5c3eabb25b2e4be5ba86fa4747aaccfe0e22d43d
nasir-rahman9 <nasir.rahman@planninginspectorate.gov.uk>
2024-03-01 08:53:00 +0000

    fix error on output

--- app/modules/app-service-front-door/outputs.tf
+++ app/modules/app-service-front-door/outputs.tf
@@ -1,6 +1,4 @@
 output "front_door_profile_id" {
-  count = var.environment == "dev" ? 1 : 0
-
   description = "ID of the Azure Front Door Profile"
-  value       = azurerm_cdn_frontdoor_profile.default[count.index].id
+  value       = azurerm_cdn_frontdoor_profile.default[0].id
 }
