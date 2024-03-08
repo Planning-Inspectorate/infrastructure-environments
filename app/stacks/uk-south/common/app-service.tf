13c62ea65e331bfe1d170050174ebca6718b54ce
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-08 14:44:44 +0000

    added skips: TODO: 212 and 225 to add minimum number of instances for a failover and to ensure App services are zone redundant

--- app/stacks/uk-south/common/app-service.tf
+++ app/stacks/uk-south/common/app-service.tf
@@ -1,4 +1,6 @@
 resource "azurerm_service_plan" "common_service_plan" {
+  #checkov:skip=CKV_AZURE_212: TODO: Ensure App Service has a minimum number of instances for failover
+  #checkov:skip=CKV_AZURE_225: TODO: Ensure the App Service Plan is zone redundant
   count = var.is_dr_deployment ? 1 : 0
 
   name                = "pins-asp-${local.service_name}-${local.resource_suffix}"
