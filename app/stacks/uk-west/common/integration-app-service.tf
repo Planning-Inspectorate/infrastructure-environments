6b989a2a54ae3e924a04856198077691d53cc4f1
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-08 16:00:00 +0000

    Added a skips: TODO: for app service plans to ensure zone redundancy and minimum number of instances for failover

--- app/stacks/uk-west/common/integration-app-service.tf
+++ app/stacks/uk-west/common/integration-app-service.tf
@@ -1,4 +1,6 @@
 resource "azurerm_service_plan" "common_integration_functions_service_plan" {
+  #checkov:skip=CKV_AZURE_212: TODO: Ensure App Service has a minimum number of instances for failover
+  #checkov:skip=CKV_AZURE_225: TODO: Ensure the App Service Plan is zone redundant
   name                = "pins-asp-integration-functions-${local.service_name}-${local.resource_suffix}"
   location            = azurerm_resource_group.common_infrastructure.location
   resource_group_name = azurerm_resource_group.common_infrastructure.name
