3fb9c88fcd80ce1f6aafd4998a5e878d94c0f072
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-08 15:55:41 +0000

    Added a TODO: as a reminder to upgrade the claranet/regions/azurerm to the latest version

--- app/stacks/uk-west/applications-service/region.tf
+++ app/stacks/uk-west/applications-service/region.tf
@@ -1,5 +1,6 @@
 module "azure_region_ukw" {
   #checkov:skip=CKV_TF_1: Use of commit hash is not required for this module, referencing the official Hashicorp module.
+  #TODO: Upgrade to latest version when possible
   source  = "claranet/regions/azurerm"
   version = "4.2.1"
 
