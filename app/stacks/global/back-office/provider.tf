381193be8ecacef5417083886ccd07b02eff46b7
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-15 11:45:51 +0000

    updated providers

--- app/stacks/global/back-office/provider.tf
+++ app/stacks/global/back-office/provider.tf
@@ -1,10 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.96.0"
-    }
-  }
+provider "azurerm" {
+  features {}
 }
 
 provider "azurerm" {
