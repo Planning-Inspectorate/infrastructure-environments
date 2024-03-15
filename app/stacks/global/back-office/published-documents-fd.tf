c3594a8ecb0e78122b798fd702f2ac8d65dcb242
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-15 15:01:58 +0000

    testing code for PR

--- app/stacks/global/back-office/published-documents-fd.tf
+++ app/stacks/global/back-office/published-documents-fd.tf
@@ -8,10 +8,10 @@ module "published_documents_front_door" {
   common_tags = local.tags
   environment = "dev"
   # enable_search_indexing_by_default = var.enable_search_indexing_by_default
-  location            = var.location # Needed for RG
-  name                = "published_documents-fd"
-  service_name        = "back-office"
-  sku_name            = "Standard_AzureFrontDoor"
+  location     = var.location # Needed for RG
+  name         = "published_documents-fd"
+  service_name = "back-office"
+  sku_name     = "Standard_AzureFrontDoor"
 
   providers = {
     azurerm         = azurerm
