5959ef56f0d56ff001ceab05a82e544be0c2aa8f
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-18 14:41:50 +0000

    adjusted rg name

--- app/stacks/global/back-office/resource-group.tf
+++ app/stacks/global/back-office/resource-group.tf
@@ -1,11 +1,11 @@
 # TODO: Create a shared FD resource group in the global stack
 resource "azurerm_resource_group" "frontdoor" {
-  name     = "${var.environment}-rg"
+  name     = "${var.environment}"
   location = module.azure_region_uks.location
 
   tags = merge(
     local.tags,
-  feature/BOAS-113-add-group-IDs-to-the-deployment-containers  {
+    {
       Region = module.azure_region_uks.location
     }
   )
