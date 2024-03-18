0588f8427aa3622ff03e04d8c047d1e823e7fe63
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-18 14:02:13 +0000

    adjusted rg name

--- app/stacks/global/back-office/resource-group.tf
+++ app/stacks/global/back-office/resource-group.tf
@@ -1,6 +1,6 @@
 # TODO: Create a shared FD resource group in the global stack
 resource "azurerm_resource_group" "frontdoor" {
-  name     = "pins-rg-front-door-${var.environment}-${module.azure_region_uks.location_short}"
+  name     = "${var.environment}-${module.azure_region_uks.location_short}"
   location = module.azure_region_uks.location
 
   tags = merge(
