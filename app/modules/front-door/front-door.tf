2a8edda0803d83c5d3f9b69b2949d4c68a9f138e
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-18 16:27:20 +0000

    added domain

--- app/modules/front-door/front-door.tf
+++ app/modules/front-door/front-door.tf
@@ -37,7 +37,7 @@ resource "azurerm_cdn_frontdoor_origin" "default" {
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.default.id
 
   enabled                        = true
-  host_name                      = var.host_name
+  host_name                      = var.domain_name
   certificate_name_check_enabled = true
 }
 
