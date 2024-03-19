4113ea7da2f8e1519c293f875b1626674d18228f
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-19 17:08:23 +0000

    cleaning up resources for module

--- app/stacks/global/back-office/data.tf
+++ app/stacks/global/back-office/data.tf
@@ -1,7 +1,3 @@
-data "azurerm_resource_group" "pins_rg_back_office" {
-  name = "pins-rg-back-office-dev-ukw-001"
-}
-
 data "azurerm_storage_account" "pinsstdocsbodevukw001" {
   name                = "pinsstdocsbodevukw001"
   resource_group_name = "pins-rg-back-office-dev-ukw-001"
--- app/stacks/global/back-office/published-documents-fd.tf
+++ app/stacks/global/back-office/published-documents-fd.tf
@@ -10,7 +10,6 @@ module "published_documents_front_door" {
   # enable_search_indexing_by_default = var.enable_search_indexing_by_default
   location            = var.location # Needed for RG
   name                = "publisheddocumentsfd"
-  resource_group_name = data.azurerm_resource_group.pins_rg_back_office.name
   service_name        = "back-office"
   sku_name            = "Premium_AzureFrontDoor"
   storage_account_id  = data.azurerm_storage_account.pinsstdocsbodevukw001.id
