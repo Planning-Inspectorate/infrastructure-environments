f0274348bc861ac5a9bbdcb9cea1687a75b7411d
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-19 15:59:37 +0000

    Upgraded to premium to enable use of private link.

--- app/modules/front-door/variable.tf
+++ app/modules/front-door/variable.tf
@@ -43,6 +43,16 @@ variable "service_name" {
   type        = string
 }
 
+variable "storage_account_primary_location" {
+  description = "The location of where the Storage Accounts are deployed."
+  type        = string
+}
+
+variable "storage_account_id" {
+  description = "The id of the Storage Accounts are to enable private link."
+  type        = string
+}
+
 variable "sku_name" {
   description = "Specifies the SKU for this Front Door Profile."
   type        = string
