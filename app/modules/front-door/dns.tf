2c9685efed6b0ab7471fac06fb10030a1d5d7091
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-19 15:59:37 +0000

    Upgraded to premium to enable use of private link.

--- app/modules/front-door/dns.tf
+++ app/modules/front-door/dns.tf
@@ -1,9 +1,9 @@
-resource "azurerm_dns_cname_record" "default" {
-  depends_on = [azurerm_cdn_frontdoor_route.default]
+# resource "azurerm_dns_cname_record" "default" {
+#   depends_on = [azurerm_cdn_frontdoor_route.default]
 
-  name                = var.service_name
-  zone_name           = azurerm_dns_zone.default.name
-  resource_group_name = azurerm_resource_group.frontdoor.name
-  ttl                 = 3600
-  record              = azurerm_cdn_frontdoor_endpoint.default.host_name
-}
+#   name                = var.service_name
+#   zone_name           = azurerm_dns_zone.default.name
+#   resource_group_name = azurerm_resource_group.frontdoor.name
+#   ttl                 = 3600
+#   record              = azurerm_cdn_frontdoor_endpoint.default.host_name
+# }
