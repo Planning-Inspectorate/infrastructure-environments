7b050f76a375998af7ccbc3445a1efbebb9c7854
nasir-rahman9 <122629783+nasir-rahman9@users.noreply.github.com>
2024-03-20 09:24:40 +0000

    Reverting code to standard to test code (#722)
    * Reverting code to standard to test code
    
    * removed redundant rg variable

--- app/stacks/global/back-office/data.tf
+++ app/stacks/global/back-office/data.tf
@@ -1,8 +1,4 @@
-data "azurerm_resource_group" "pins_rg_back_office" {
-  name = "pins-rg-back-office-dev-ukw-001"
-}
-
-data "azurerm_storage_account" "pinsstdocsbodevukw001" {
-  name                = "pinsstdocsbodevukw001"
-  resource_group_name = "pins-rg-back-office-dev-ukw-001"
-}
+# data "azurerm_storage_account" "pinsstdocsbodevukw001" {
+#   name                = "pinsstdocsbodevukw001"
+#   resource_group_name = "pins-rg-back-office-dev-ukw-001"
+# }
