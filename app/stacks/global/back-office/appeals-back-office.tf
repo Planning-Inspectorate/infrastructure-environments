a7fab9aa5719ab23d1a85c20f2b3ef7410e47953
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-27 16:05:25 +0000

    updated variables to fix pipeline error

--- app/stacks/global/back-office/appeals-back-office.tf
+++ app/stacks/global/back-office/appeals-back-office.tf
@@ -1,12 +1,12 @@
 module "appeals_back_office" {
   source = "../../../components/back-office-app-services/appeals-back-office-fd"
 
-  # domain_name       = var.back_office_appeals_public_url
+  domain_name = var.back_office_appeals_public_url
   # common_tags       = local.tags
   # environment       = var.environment
-  # frontend_endpoint = var.back_office_appeals_primary_app_service_url
+  frontend_endpoint = var.back_office_appeals_primary_app_service_url
   # location          = var.location
-  # name              = "${var.appeals_back_office_service_name}-${var.environment}"
+  name = "${var.appeals_back_office_service_name}-${var.environment}"
   # sku_name          = "Premium_AzureFrontDoor"
   # origin            = var.origin
   # route             = var.route
