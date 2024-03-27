516d6ca58183d311af61fee519df662859c6e4b5
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-27 15:28:02 +0000

    hashed out terraform arguments

--- app/stacks/global/back-office/appeals-back-office.tf
+++ app/stacks/global/back-office/appeals-back-office.tf
@@ -1,15 +1,15 @@
 module "appeals_back_office" {
   source = "../../../components/back-office-app-services/appeals-back-office-fd"
 
-  domain_name       = var.back_office_appeals_public_url
-  common_tags       = local.tags
-  environment       = var.environment
-  frontend_endpoint = var.back_office_appeals_primary_app_service_url
-  location          = var.location
-  name              = "${var.appeals_back_office_service_name}-${var.environment}"
-  sku_name          = "Premium_AzureFrontDoor"
-  origin            = var.origin
-  route             = var.route
+  # domain_name       = var.back_office_appeals_public_url
+  # common_tags       = local.tags
+  # environment       = var.environment
+  # frontend_endpoint = var.back_office_appeals_primary_app_service_url
+  # location          = var.location
+  # name              = "${var.appeals_back_office_service_name}-${var.environment}"
+  # sku_name          = "Premium_AzureFrontDoor"
+  # origin            = var.origin
+  # route             = var.route
 
   providers = {
     azurerm         = azurerm
