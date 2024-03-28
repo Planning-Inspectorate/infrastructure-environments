27c07f1065588254e84436bf88020026ea9e89a2
nasir-rahman9 <nasirrahman@live.co.uk>
2024-03-28 10:27:08 +0000

    fix to add the arguments and variables to the stacks

--- app/stacks/global/back-office/variables.tf
+++ app/stacks/global/back-office/variables.tf
@@ -1,13 +1,13 @@
-# variable "appeals_back_office_service_name" {
-#   description = "The service name used for appeals back office"
-#   type        = string
-#   default     = "appeals-back-office"
-# }
+variable "appeals_back_office_service_name" {
+  description = "The service name used for appeals back office"
+  type        = string
+  default     = "appeals-back-office"
+}
 
-# variable "back_office_appeals_public_url" {
-#   description = "The public URL for the Back Office Appeals frontend web app"
-#   type        = string
-# }
+variable "back_office_appeals_public_url" {
+  description = "The public URL for the Back Office Appeals frontend web app"
+  type        = string
+}
 
 variable "common_tags" {
   description = "The common resource tags for the project"
@@ -46,7 +46,63 @@ variable "back_office_published_documents_domain" {
   type        = string
 }
 
-# variable "back_office_appeals_primary_app_service_url" {
-#   description = "The primary App Service URL for the Back Office Appeals"
-#   type        = string
-# }
+variable "back_office_appeals_primary_app_service_url" {
+  description = "The primary App Service URL for the Back Office Appeals"
+  type        = string
+}
+
+variable "origin" {
+  description = "CDN FrontDoor Origins configurations."
+  type = map(object({
+    name                           = string
+    custom_resource_name           = optional(string)
+    origin_group_name              = string
+    enabled                        = optional(bool, true)
+    certificate_name_check_enabled = optional(bool, true)
+
+    host_name          = string
+    http_port          = optional(number, 80)
+    https_port         = optional(number, 443)
+    origin_host_header = optional(string)
+    priority           = optional(number, 5)
+    weight             = optional(number, 100)
+
+    private_link = optional(object({
+      request_message        = optional(string)
+      target_type            = optional(string)
+      location               = string
+      private_link_target_id = string
+    }))
+  }))
+  default = {}
+}
+
+variable "route" {
+  description = "CDN FrontDoor Routes configurations."
+  type = map(object({
+    name                 = string
+    custom_resource_name = optional(string)
+    enabled              = optional(bool, true)
+
+    endpoint_name     = string
+    origin_group_name = string
+    origins_names     = list(string)
+
+    forwarding_protocol = optional(string, "MatchRequest")
+    patterns_to_match   = optional(list(string), ["/*"])
+    supported_protocols = optional(list(string), ["Https"])
+    cache = optional(object({
+      query_string_caching_behavior = optional(string, "IgnoreQueryString")
+      query_strings                 = optional(list(string))
+      compression_enabled           = optional(bool, false)
+      content_types_to_compress     = optional(list(string))
+    }))
+
+    custom_domains_names = optional(list(string), [])
+    origin_path          = optional(string)
+    rule_sets_names      = optional(list(string), [])
+
+    https_redirect_enabled = optional(bool, true)
+    link_to_default_domain = optional(bool, true)
+  }))
+}
