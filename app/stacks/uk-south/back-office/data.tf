5a2c60633ac54ba5b92320425245cc42367d7654
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2024-04-16 15:47:02 +0100

    Add Azure App Configuration code to uk-south for back-office

--- app/stacks/uk-south/back-office/data.tf
+++ app/stacks/uk-south/back-office/data.tf
@@ -1,3 +1,9 @@
+data "azurerm_private_dns_zone" "app_config" {
+  name = "privatelink.azconfig.io"
+
+  provider = azurerm.tooling
+}
+
 data "azurerm_private_dns_zone" "app_service" {
   name = "privatelink.azurewebsites.net"
 
