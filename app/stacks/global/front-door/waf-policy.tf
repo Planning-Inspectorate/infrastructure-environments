731dc398bd675320d9bd1302744a9adbaa9137b7
Philip-Philippou1 <philip.philippou1@gmail.com>
2024-05-20 10:27:58 +0100

    fix: move exclusions into BOAS firewall

--- app/stacks/global/front-door/waf-policy.tf
+++ app/stacks/global/front-door/waf-policy.tf
@@ -148,27 +148,6 @@ resource "azurerm_frontdoor_firewall_policy" "default" {
       operator       = "Equals"
       selector       = "backOfficeProjectUpdateContent"
     }
-
-    # Exclusions for APPLICS-380 - Exclude all rules for this selector.
-    # POST BO project description and locationDescription, POST document fileName
-    # only applies Back Office fields.
-    exclusion {
-      match_variable = "RequestBodyPostArgNames"
-      operator       = "Equals"
-      selector       = "geographicalInformation.locationDescription"
-    }
-
-    exclusion {
-      match_variable = "RequestBodyPostArgNames"
-      operator       = "Equals"
-      selector       = "fileName"
-    }
-
-    exclusion {
-      match_variable = "RequestBodyPostArgNames"
-      operator       = "Equals"
-      selector       = "description"
-    }
   }
 
   custom_rule {
@@ -589,6 +568,27 @@ resource "azurerm_frontdoor_firewall_policy" "back_office_applications_frontend"
       match_variable = "RequestBodyPostArgNames"
       operator       = "Equals"
       selector       = "backOfficeProjectUpdateContent"
+    }
+    
+    # Exclusions for APPLICS-380 - Exclude all rules for this selector.
+    # POST BO project description and locationDescription, POST document fileName
+    # only applies Back Office fields.
+    exclusion {
+      match_variable = "RequestBodyPostArgNames"
+      operator       = "Equals"
+      selector       = "geographicalInformation.locationDescription"
+    }
+
+    exclusion {
+      match_variable = "RequestBodyPostArgNames"
+      operator       = "Equals"
+      selector       = "fileName"
+    }
+
+    exclusion {
+      match_variable = "RequestBodyPostArgNames"
+      operator       = "Equals"
+      selector       = "description"
     }
   }
 
