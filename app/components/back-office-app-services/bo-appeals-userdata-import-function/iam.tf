1cd244eb925e349021d325255c572f74962960a1
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-05-23 17:02:47 +0100

    chore(back-office): remove appeals functions

--- app/components/back-office-app-services/bo-appeals-userdata-import-function/iam.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_role_assignment" "service_bus_data_receiver_service_user" {
-  scope                = azurerm_servicebus_subscription.register_odw_serviceuser_subscription.id
-  role_definition_name = "Azure Service Bus Data Receiver"
-  principal_id         = module.bo_appeals_user_import_function.principal_id
-}
