c34ba66aef034b475d065ba6632df82df3e4c9ed
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-10-21 16:00:42 +0100

    fix(front-door): re-arrange custom rules to match the API

--- app/stacks/global/front-door/waf-policy.tf
+++ app/stacks/global/front-door/waf-policy.tf
@@ -150,6 +150,24 @@ resource "azurerm_frontdoor_firewall_policy" "default" {
     }
   }
 
+  # custom rules in priority order to match the API
+  custom_rule {
+    name     = "IpBlock"
+    action   = "Block"
+    enabled  = true
+    priority = 10
+    type     = "MatchRule"
+
+    match_condition {
+      match_variable     = "RemoteAddr"
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values = [
+        "10.255.255.255" # placeholder value
+      ]
+    }
+  }
+
   custom_rule {
     name                           = "RateLimitHttpRequest"
     action                         = "Block"
@@ -192,30 +210,13 @@ resource "azurerm_frontdoor_firewall_policy" "default" {
     }
   }
 
-  custom_rule {
-    name     = "IpBlock"
-    action   = "Block"
-    enabled  = true
-    priority = 10
-    type     = "MatchRule"
-
-    match_condition {
-      match_variable     = "RemoteAddr"
-      operator           = "IPMatch"
-      negation_condition = false
-      match_values = [
-        "10.255.255.255" # placeholder value
-      ]
-    }
-  }
-
   tags = var.common_tags
 
   lifecycle {
     ignore_changes = [
-      # match the third custom rule (IpBlock) and ignore the match values (IPs)
+      # match the first custom rule (IpBlock) and ignore the match values (IPs)
       # managed in Portal
-      custom_rule[2].match_condition[0].match_values
+      custom_rule[0].match_condition[0].match_values
     ]
   }
 }
@@ -414,6 +415,24 @@ resource "azurerm_frontdoor_firewall_policy" "appeals_frontend" {
     }
   }
 
+  # custom rules in priority order to match the API
+  custom_rule {
+    name     = "IpBlock"
+    action   = "Block"
+    enabled  = true
+    priority = 10
+    type     = "MatchRule"
+
+    match_condition {
+      match_variable     = "RemoteAddr"
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values = [
+        "10.255.255.255" # placeholder value
+      ]
+    }
+  }
+
   custom_rule {
     name                           = "RateLimitHttpRequest"
     action                         = "Block"
@@ -439,30 +458,13 @@ resource "azurerm_frontdoor_firewall_policy" "appeals_frontend" {
     }
   }
 
-  custom_rule {
-    name     = "IpBlock"
-    action   = "Block"
-    enabled  = true
-    priority = 10
-    type     = "MatchRule"
-
-    match_condition {
-      match_variable     = "RemoteAddr"
-      operator           = "IPMatch"
-      negation_condition = false
-      match_values = [
-        "10.255.255.255" # placeholder value
-      ]
-    }
-  }
-
   tags = var.common_tags
 
   lifecycle {
     ignore_changes = [
-      # match the second custom rule (IpBlock) and ignore the match values (IPs)
+      # match the first custom rule (IpBlock) and ignore the match values (IPs)
       # managed in Portal
-      custom_rule[1].match_condition[0].match_values
+      custom_rule[0].match_condition[0].match_values
     ]
   }
 }
@@ -702,6 +704,24 @@ resource "azurerm_frontdoor_firewall_policy" "back_office_applications_frontend"
     }
   }
 
+  # custom rules in priority order to match the API
+  custom_rule {
+    name     = "IpBlock"
+    action   = "Block"
+    enabled  = true
+    priority = 10
+    type     = "MatchRule"
+
+    match_condition {
+      match_variable     = "RemoteAddr"
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values = [
+        "10.255.255.255" # placeholder value
+      ]
+    }
+  }
+
   custom_rule {
     name                           = "RateLimitHttpRequest"
     action                         = "Block"
@@ -727,30 +747,13 @@ resource "azurerm_frontdoor_firewall_policy" "back_office_applications_frontend"
     }
   }
 
-  custom_rule {
-    name     = "IpBlock"
-    action   = "Block"
-    enabled  = true
-    priority = 10
-    type     = "MatchRule"
-
-    match_condition {
-      match_variable     = "RemoteAddr"
-      operator           = "IPMatch"
-      negation_condition = false
-      match_values = [
-        "10.255.255.255" # placeholder value
-      ]
-    }
-  }
-
   tags = var.common_tags
 
   lifecycle {
     ignore_changes = [
-      # match the second custom rule (IpBlock) and ignore the match values (IPs)
+      # match the first custom rule (IpBlock) and ignore the match values (IPs)
       # managed in Portal
-      custom_rule[1].match_condition[0].match_values
+      custom_rule[0].match_condition[0].match_values
     ]
   }
 }
