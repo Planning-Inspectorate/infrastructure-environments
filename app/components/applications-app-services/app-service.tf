51fef6f595c9b8cf92334df9e00e477fab182265
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2025-01-13 15:06:59 +0000

    chore(tooling): upgrading to  azurerm_4

--- app/components/applications-app-services/app-service.tf
+++ app/components/applications-app-services/app-service.tf
@@ -4,26 +4,26 @@ module "app_service" {
 
   source = "github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-app-service?ref=1.33"
 
-  action_group_ids                = var.action_group_ids
-  app_name                        = each.value["app_name"]
-  app_service_plan_id             = var.app_service_plan_id
-  app_service_private_dns_zone_id = can(each.value["app_service_private_dns_zone_id"]) ? each.value["app_service_private_dns_zone_id"] : null
-  app_settings                    = each.value["app_settings"]
-  container_registry_name         = var.container_registry_name
-  container_registry_rg           = var.container_registry_rg
-  endpoint_subnet_id              = can(each.value["endpoint_subnet_id"]) ? each.value["endpoint_subnet_id"] : null
-  front_door_restriction          = can(each.value["front_door_restriction"]) ? each.value["front_door_restriction"] : null
-  image_name                      = each.value["image_name"]
-  inbound_vnet_connectivity       = each.value["inbound_vnet_connectivity"]
-  integration_subnet_id           = can(each.value["integration_subnet_id"]) ? each.value["integration_subnet_id"] : null
-  key_vault_id                    = each.value["key_vault_access"] ? var.key_vault_id : null
-  location                        = var.location
-  log_analytics_workspace_id      = var.log_analytics_workspace_id
-  monitoring_alerts_enabled       = var.monitoring_alerts_enabled
-  outbound_vnet_connectivity      = each.value["outbound_vnet_connectivity"]
-  resource_group_name             = var.resource_group_name
-  resource_suffix                 = var.resource_suffix
-  service_name                    = var.service_name
+  action_group_ids                  = var.action_group_ids
+  app_name                          = each.value["app_name"]
+  app_service_plan_id               = var.app_service_plan_id
+  app_service_private_dns_zone_id   = can(each.value["app_service_private_dns_zone_id"]) ? each.value["app_service_private_dns_zone_id"] : null
+  app_settings                      = each.value["app_settings"]
+  container_registry_name           = var.container_registry_name
+  container_registry_rg             = var.container_registry_rg
+  endpoint_subnet_id                = can(each.value["endpoint_subnet_id"]) ? each.value["endpoint_subnet_id"] : null
+  front_door_restriction            = can(each.value["front_door_restriction"]) ? each.value["front_door_restriction"] : null
+  image_name                        = each.value["image_name"]
+  inbound_vnet_connectivity         = each.value["inbound_vnet_connectivity"]
+  integration_subnet_id             = can(each.value["integration_subnet_id"]) ? each.value["integration_subnet_id"] : null
+  key_vault_id                      = each.value["key_vault_access"] ? var.key_vault_id : null
+  location                          = var.location
+  log_analytics_workspace_id        = var.log_analytics_workspace_id
+  monitoring_alerts_enabled         = var.monitoring_alerts_enabled
+  outbound_vnet_connectivity        = each.value["outbound_vnet_connectivity"]
+  resource_group_name               = var.resource_group_name
+  resource_suffix                   = var.resource_suffix
+  service_name                      = var.service_name
   health_check_eviction_time_in_min = var.health_check_eviction_time_in_min
   auth_config                       = ""
 
