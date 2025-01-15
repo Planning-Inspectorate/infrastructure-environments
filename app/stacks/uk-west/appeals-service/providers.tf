1c6609cf7f1fd58387533196bf06c3a341096d0f
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2025-01-15 13:09:13 +0000

    chore(tooling): upgrading to  azurerm_4

--- app/stacks/uk-west/appeals-service/providers.tf
+++ app/stacks/uk-west/appeals-service/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">3.107.0"
+      version = "> 3.107.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = "~>0.9"
+      version = "~> 0.9"
     }
   }
 
