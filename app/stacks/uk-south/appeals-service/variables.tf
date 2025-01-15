9ff8087bfca4cd42a24558cfb991a244434eddae
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2025-01-15 14:59:26 +0000

    chore(tooling): upgrading to  azurerm_4

--- app/stacks/uk-south/appeals-service/variables.tf
+++ app/stacks/uk-south/appeals-service/variables.tf
@@ -46,6 +46,20 @@ variable "appeal_documents_primary_blob_host" {
   type        = string
 }
 
+#EasyAuth setting
+variable "auth_config" {
+  description = "Config for the Azure Easy Authentication"
+  type = object({
+    auth_enabled           = bool
+    require_authentication = bool
+    auth_client_id         = string
+    auth_provider_secret   = string
+    auth_tenant_endpoint   = string
+    allowed_audiences      = string
+    allowed_applications   = string
+  })
+}
+
 variable "appeals_service_public_url" {
   description = "The public URL for the Appeals Service frontend web app"
   type        = string
