1d6ac3b693f2d06667962547476f0dd01032e8ef
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-03-26 16:28:26 +0000

    feat(appeals): bump node version for function app

--- app/components/appeals-app-services/function-app.tf
+++ app/components/appeals-app-services/function-app.tf
@@ -20,7 +20,7 @@ module "front_office_subscribers" {
   resource_group_name                      = var.resource_group_name
   resource_suffix                          = var.resource_suffix
   service_name                             = "appeals"
-  function_node_version                    = 20
+  function_node_version                    = 22
 
   app_settings = {
     ServiceBusConnection__fullyQualifiedNamespace = "${var.back_office_service_bus_namespace_name}.servicebus.windows.net"
