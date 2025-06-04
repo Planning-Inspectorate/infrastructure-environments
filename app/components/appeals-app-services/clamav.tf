7a0b72cb448c9bc5ac9373d345493e8cda688d15
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2025-06-04 13:12:46 +0100

    feat(appeals): increase clamAV container group memory to 6GB

--- app/components/appeals-app-services/clamav.tf
+++ app/components/appeals-app-services/clamav.tf
@@ -15,7 +15,7 @@ resource "azurerm_container_group" "clamav" {
     name   = "clamav"
     image  = "clamav/clamav:latest_base"
     cpu    = "1"
-    memory = "4"
+    memory = "6"
 
     volume {
       name       = "clamav-db"
