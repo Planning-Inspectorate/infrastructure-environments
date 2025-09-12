cee9b76879591ee034b6dc11bf10720a681d8fc2
Medini Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2025-09-12 11:38:15 +0100

    fix(network): testing static allocation for public IP

--- app/components/networking/vpn-gateway.tf
+++ app/components/networking/vpn-gateway.tf
@@ -5,7 +5,7 @@ resource "azurerm_public_ip" "vnet_gateway" {
   resource_group_name = var.resource_group_name
   location            = var.location
   allocation_method   = "Static"
-  sku                 = "Standard"
+  sku                 = "Basic"
 
   tags = var.tags
 }
