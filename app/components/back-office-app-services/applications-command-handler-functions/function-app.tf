82df8c1eff5ba9c9017616e166c896ebbb66e34f
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-10-02 14:57:00 +0100

    fix: update node-function-app#1.51 to ensure function logs are set to Information
    also update node-app-service to keep in sync

--- app/components/back-office-app-services/applications-command-handler-functions/function-app.tf
+++ app/components/back-office-app-services/applications-command-handler-functions/function-app.tf
@@ -1,6 +1,6 @@
 module "applications_command_handler_functions" {
   #checkov:skip=CKV_TF_1: Use of commit hash are not required for our Terraform modules
-  source = "github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-function-app?ref=1.50"
+  source = "github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-function-app?ref=1.51"
 
   action_group_ids                         = var.action_group_ids
   app_name                                 = "apps-command-handlers"
