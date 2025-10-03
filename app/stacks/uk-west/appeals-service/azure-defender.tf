0fd3d57c2827908050acc38f02448de805e7c753
Medini Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2025-10-03 11:22:21 +0100

    fix(infrastructure): adding count condition threat protection

--- app/stacks/uk-west/appeals-service/azure-defender.tf
+++ app/stacks/uk-west/appeals-service/azure-defender.tf
@@ -1,4 +1,10 @@
+moved {
+  from = azurerm_advanced_threat_protection.appeal_documents
+  to   = azurerm_advanced_threat_protection.appeal_documents[0]
+}
+
 resource "azurerm_advanced_threat_protection" "appeal_documents" {
+  count              = var.environment != "staging" ? 1 : 0
   target_resource_id = azurerm_storage_account.appeal_documents.id
   enabled            = true
 }
