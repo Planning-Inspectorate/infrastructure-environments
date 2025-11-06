a15e09ce82b20b1f2a79ee5c637249be80ff63ff
Camilla Priest-Stephens <camilla.priest-stephens@planninginspectorate.gov.uk>
2025-11-06 14:23:07 +0000

    feat(applications): create projects map feature flags (applics-1812)

--- app/stacks/uk-west/applications-service/app-service.tf
+++ app/stacks/uk-west/applications-service/app-service.tf
@@ -49,6 +49,7 @@ module "app_services" {
   feature_redis_session_store                                           = var.feature_redis_session_store
   feature_save_and_exit_option                                          = var.feature_save_and_exit_option
   feature_show_affected_area_section                                    = var.feature_show_affected_area_section
+  feature_enable_projects_map                                           = var.feature_enable_projects_map
   function_storage_name                                                 = azurerm_storage_account.function_storage.name
   function_storage_primary_access_key                                   = azurerm_storage_account.function_storage.primary_access_key
   global_banner_text                                                    = var.global_banner_text
