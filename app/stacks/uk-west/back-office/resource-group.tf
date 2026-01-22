b981e98b27baafc3d51bf32d2b9eb5b662ef83bb
Medini Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2026-01-22 13:50:44 +0000

    fix(infrastructure): adding import statement for back office rg

--- app/stacks/uk-west/back-office/resource-group.tf
+++ app/stacks/uk-west/back-office/resource-group.tf
@@ -1,3 +1,8 @@
+import {
+  id = "/subscriptions/962e477c-0f3b-4372-97fc-a198a58e259e/resourceGroups/pins-rg-back-office-dev-ukw-001"
+  to = azurerm_resource_group.back_office_stack
+}
+
 resource "azurerm_resource_group" "back_office_stack" {
   name     = "pins-rg-${local.service_name}-${local.resource_suffix}"
   location = module.azure_region_ukw.location
