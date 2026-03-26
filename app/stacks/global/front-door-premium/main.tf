0093d3cf71e691411e3915e5fad79281f03ed558
Snasim21 <Sabbir.Nasim.47@planninginspectorate.gov.uk>
2026-03-26 09:44:39 +0000

    feat(infrastructure): update log cap alert to use variables for limit and service name

--- app/stacks/global/front-door-premium/main.tf
+++ app/stacks/global/front-door-premium/main.tf
@@ -77,7 +77,7 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "log_cap" {
   count = var.environment == "prod" ? 1 : 0
 
   name         = "Log cap Alert"
-  display_name = "log Daily data limit reached"
+  display_name = "Daily logging limit (${var.log_daily_cap_gb}GB) reached for Front Door in PROD"
   description  = "Triggered when the log Data cap is reached."
 
   location            = module.azure_region_ukw.location
