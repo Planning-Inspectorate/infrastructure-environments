9a9ef2154b75bfa7efcd4b7f60deb0e51c68634e
Emil Placheta <emil.placheta@planninginspectorate.gov.uk>
2026-03-30 11:33:12 +0100

    fix(CBOS): add import statement for diagnostic setting

--- app/stacks/uk-west/back-office/monitoring.tf
+++ app/stacks/uk-west/back-office/monitoring.tf
@@ -19,7 +19,7 @@ resource "azurerm_log_analytics_workspace" "back_office" {
 }
 
 import {
-  id = "/subscriptions/d1d6c393-2fe3-40af-ac27-f5b6bad36735/resourceGroups/pins-rg-back-office-prod-ukw-001/providers/Microsoft.OperationalInsights/workspaces/pins-log-back-office-prod-ukw-001"
+  id = "/subscriptions/d1d6c393-2fe3-40af-ac27-f5b6bad36735/resourceGroups/pins-rg-back-office-prod-ukw-001/providers/Microsoft.Storage/storageAccounts/pinsstdocsboprodukw001/blobServices/default|pins-documents-back-office-prod-ukw-001"
   to = azurerm_monitor_diagnostic_setting.back_office_documents
 }
 
