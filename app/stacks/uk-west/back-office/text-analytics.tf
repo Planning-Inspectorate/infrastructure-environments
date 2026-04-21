52e389ebc32d461a15839da3187d2f197280ee8d
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-04-21 09:14:36 +0100

    feat(back-office): add second language service for testing

--- app/stacks/uk-west/back-office/text-analytics.tf
+++ app/stacks/uk-west/back-office/text-analytics.tf
@@ -18,7 +18,7 @@ data "azurerm_cognitive_account" "text_analytics" {
   provider = azurerm.tooling
 }
 
-# Crown specific instance for live environment
+# Applications specific instance for live environment
 resource "azurerm_cognitive_account" "text_analytics" {
   count = var.text_analytics_config.deploy ? 1 : 0
 
@@ -56,6 +56,50 @@ resource "azurerm_private_endpoint" "text_analytics_private_endpoint" {
   private_service_connection {
     name                           = "privateendpointconnection"
     private_connection_resource_id = azurerm_cognitive_account.text_analytics[0].id
+    subresource_names              = ["account"]
+    is_manual_connection           = false
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_cognitive_account" "text_analytics_v2" {
+  count = var.text_analytics_config.deploy ? 1 : 0
+
+  #checkov:skip=CKV2_AZURE_22: customer-managed keys not implemented yet
+  name                          = "pins-lang-cbos-${local.resource_suffix}"
+  location                      = azurerm_resource_group.back_office_stack.location
+  resource_group_name           = azurerm_resource_group.back_office_stack.name
+  kind                          = "TextAnalytics"
+  public_network_access_enabled = false
+  local_auth_enabled            = false
+  custom_subdomain_name         = "pins-lang-cbos-${local.resource_suffix}"
+
+  sku_name = "S"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_private_endpoint" "text_analytics_private_endpoint_v2" {
+  count = var.text_analytics_config.deploy ? 1 : 0
+
+  name                = "pins-pe-lang-cbos-${local.resource_suffix}"
+  location            = azurerm_resource_group.back_office_stack.location
+  resource_group_name = azurerm_resource_group.back_office_stack.name
+  subnet_id           = azurerm_subnet.back_office_ingress.id
+
+  private_dns_zone_group {
+    name                 = "azure-lang-private-dns-zone-group"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.cognitive.id]
+  }
+
+  private_service_connection {
+    name                           = "privateendpointconnection"
+    private_connection_resource_id = azurerm_cognitive_account.text_analytics_v2[0].id
     subresource_names              = ["account"]
     is_manual_connection           = false
   }
