e7ab3bab4e817516c4f4b18c2582e235eecd6e48
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2026-04-30 16:12:54 +0100

    chore(infrastructure): adding vnet link to storage private dns zone

--- app/components/networking/data.tf
+++ app/components/networking/data.tf
@@ -11,3 +11,10 @@ data "azurerm_private_dns_zone" "keyvault" {
 
   provider = azurerm.tooling
 }
+
+data "azurerm_private_dns_zone" "storage" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = var.tooling_network_rg
+
+  provider = azurerm.tooling
+}
